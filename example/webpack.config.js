abad74e2a143695e6d6f873bad53b80b7babc0ed
Kye Hohenberger <kye@sideway.com>
2017-06-01 23:41:41 -0600

    Make deploy work

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,38 +1,48 @@
 var path = require('path')
 var webpack = require('webpack')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-var loaders = [
-  {
-    test: /\.jsx?$/,
-    exclude: /node_modules/,
-    loader: 'babel-loader'
-  },
-  {
-    test: /\.css?$/,
-    loader: 'glam/loader'
-  }
-]
+module.exports = env => {
+  const PROD = env === 'production'
 
-module.exports = {
-  devtool: 'eval-source-map',
-  entry: path.resolve('src', 'main.js'),
-  output: {
-    path: path.resolve('build'),
-    filename: 'main.js',
-    publicPath: '/'
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx']
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve('src', 'index.tpl.html'),
-      filename: 'index.html',
-      inject: false
-    })
-  ],
-  module: {
-    loaders: loaders
+  var loaders = [
+    {
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    },
+    {
+      test: /\.css$/,
+      use: PROD
+        ? ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+        : ['glam/loader']
+    }
+  ]
+
+  return {
+    devtool: PROD ? 'source-map' : 'eval-source-map',
+    entry: path.resolve('src', 'main.js'),
+    output: {
+      path: path.resolve('build'),
+      filename: 'main.js',
+      publicPath: '/'
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve('src', 'index.tpl.html'),
+        filename: 'index.html',
+        inject: false
+      })
+    ].concat(PROD ? new ExtractTextPlugin('styles.css') : []),
+    module: {
+      loaders: loaders
+    }
   }
 }
