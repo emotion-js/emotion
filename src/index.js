fe0184912076ec4338c41e9af9fc2a7e80f81dce
Kye Hohenberger <kye@sideway.com>
2017-06-23 22:03:53 -0600

    update snapshots and remove unused code

--- src/index.js
+++ src/index.js
@@ -6,29 +6,6 @@ sheet.inject()
 
 let inserted = {}
 
-function values (cls, vars) {
-  let hash = hashArray([cls, ...vars])
-  if (inserted[hash]) {
-    return `vars-${hash}`
-  }
-  let fragvarcls = []
-  let src = vars
-    .map(
-      (val, i) =>
-        `--${cls}-${i}: ${/^frag-/.exec(val)
-          ? (fragvarcls.push(val), 'var(--' + val.split(' ')[0] + ')')
-          : val}`
-    )
-    .join('; ')
-  sheet.insert(`.vars-${hash} {${src}}`)
-  inserted[hash] = true
-
-  if (fragvarcls.length > 0) {
-    return `vars-${hash} ${fragvarcls.join(' ')}`
-  }
-  return `vars-${hash}`
-}
-
 export function flush () {
   sheet.flush()
   inserted = {}
@@ -36,42 +13,36 @@ export function flush () {
 }
 
 export function css (cls, vars, content) {
-  if (content) {
-    // inline mode
-    vars = vars.map(v => (/^frag-/.exec(v) ? fragments[v] : v))
-    let src = content(...vars) // returns an array
-    let hash = hashArray(src)
+  // inline mode
+  vars = vars.map(v => (/^frag-/.exec(v) ? fragments[v] : v))
+  let src = content(...vars) // returns an array
+  let hash = hashArray(src)
 
-    if (!inserted[hash]) {
-      inserted[hash] = true
-      src
-        .map(r => r.replace(new RegExp(cls, 'gm'), `${cls}-${hash}`))
-        .forEach(r => sheet.insert(r))
-    }
-    return `${cls}-${hash}`
+  if (!inserted[hash]) {
+    inserted[hash] = true
+    src
+      .map(r => r.replace(new RegExp(cls, 'gm'), `${cls}-${hash}`))
+      .forEach(r => sheet.insert(r))
   }
-  return cls + (vars && vars.length > 0 ? ' ' + values(cls, vars) : '')
+  return `${cls}-${hash}`
 }
 
 const fragments = {}
 
 export function fragment (frag, vars, content) {
-  if (content) {
-    vars = vars.map(v => (/^frag-/.exec(v) ? fragments[v] : v))
-    let src = content(...vars) // return array?
-    if (src.length > 1) {
-      throw new Error('what up!')
-    }
-
-    let hash = hashArray(src)
-    src = src.join('')
-    fragments[`${frag}-${hash}`] = src.substring(
-      src.indexOf('{') + 1,
-      src.length - 1
-    )
-    return `${frag}-${hash}`
+  vars = vars.map(v => (/^frag-/.exec(v) ? fragments[v] : v))
+  let src = content(...vars) // return array?
+  if (src.length > 1) {
+    throw new Error('what up!')
   }
-  return frag + (vars && vars.length > 0 ? ' ' + values(frag, vars) : '')
+
+  let hash = hashArray(src)
+  src = src.join('')
+  fragments[`${frag}-${hash}`] = src.substring(
+    src.indexOf('{') + 1,
+    src.length - 1
+  )
+  return `${frag}-${hash}`
 }
 
 export function hydrate (ids) {
