f1c6fe18417d6231b5f7a8479eadb7fdb7af03df
Marvin Hagemeister <marvin@marvinhagemeister.de>
2017-06-28 08:20:26 +0200

    squash! Use proper tagged template str typigns

--- styled.d.ts
+++ styled.d.ts
@@ -1,10 +1,14 @@
 declare module "emotion/styled" {
   import { Component } from "react";
 
+  export type Styler = (
+    content: TemplateStringsArray,
+    vars?: string[] | number[],
+  ) => Component;
+
+  export default function(tag: Component): Styler;
   export default function(
-    tag: keyof HTMLElementTagNameMap | Component,
+    tag: keyof HTMLElementTagNameMap,
     cls?: string,
-    vars?: any[],
-    content?: () => string[],
-  ): Component;
+  ): Styler;
 }
