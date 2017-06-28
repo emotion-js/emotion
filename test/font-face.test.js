29abc1998313eda34dc15566112d428462b1709a
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-06-28 22:14:48 +1000

    Revert "Fix fontFace test"
    This reverts commit 1e94e48286f873e3dfc8f0f06ab1302e41c7a718.

--- test/font-face.test.js
+++ test/font-face.test.js
@@ -1,8 +1,18 @@
+/* eslint-disable jsx-quotes,no-useless-escape,no-template-curly-in-string */
 /* eslint-env jest */
-import { sheet, fontFace } from '../src/index'
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { matcher, serializer } from '../jest-utils'
 
-describe('font-face', () => {
-  test('adds font-face to sheet', () => {
+// eslint-disable-next-line no-unused-vars
+import css, { sheet, fragment, fontFace } from '../src/index'
+
+expect.addSnapshotSerializer(serializer)
+expect.extend(matcher)
+
+// This does nothing
+describe.skip('font-face', () => {
+  test('adds font-face to sheet', done => {
     fontFace`
       font-family: 'Patrick Hand SC';
       font-style: normal;
@@ -10,6 +20,7 @@ describe('font-face', () => {
       src: local('Patrick Hand SC'), local('PatrickHandSC-Regular'), url(https://fonts.gstatic.com/s/patrickhandsc/v4/OYFWCgfCR-7uHIovjUZXsZ71Uis0Qeb9Gqo8IZV7ckE.woff2) format('woff2');
       unicode-range: U+0100-024F, U+1E00-1EFF, U+20A0-20AB, U+20AD-20CF, U+2C60-2C7F, U+A720-A7FF;
     `
-    expect(sheet.tags.map((tag) => tag.textContent || '').join('')).toMatchSnapshot()
+    // console.log(sheet.rules())
+    expect(sheet).toMatchSnapshot()
   })
 })
