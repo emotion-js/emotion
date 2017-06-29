4db8d5d792d1e7b5a992e8111f498354a123e12e
Marvin Hagemeister <marvin@marvinhagemeister.de>
2017-06-29 21:43:03 +0200

    Upgrade typings to match master

--- styled.d.ts
+++ styled.d.ts
@@ -1,29 +1,22 @@
 declare module "emotion" {
+  export const inserted: Record<string, boolean | undefined>;
+
   export type InputValue = string | number;
 
+  export function values(cls: string, vars: InputValue[]): string;
+
   export function flush(): void;
+
   export function css(
     cls: string,
     content: TemplateStringsArray,
     ...vars: InputValue[],
   ): string;
 
-  export function fragment(
-    content: TemplateStringsArray,
-    ...vars: InputValue[],
-  ): string;
+  export function injectGlobal(src: string[]): void;
+  export const fontFace = injectGlobal;
 
-  export function keyframes(
-    kfm: string,
-    content: TemplateStringsArray,
-    ...vars: InputValue[],
-  ): string;
-
-  export function fontFace(
-    fontRules: string,
-    content: TemplateStringsArray,
-    ...vars: InputValue[],
-  ): string;
+  export function keyframes(kfm: string, src: string[]): string;
 
   export function hydrate(ids: string[]): void;
 }
@@ -45,4 +38,20 @@ declare module "emotion/styled" {
     tag: keyof HTMLElementTagNameMap,
     cls?: string,
   ): Tagged;
+}
+
+declare module "emotion/server" {
+  export interface Rule {
+    cssText: string;
+  }
+
+  export interface Result {
+    html: string;
+    ids: Record<string, boolean | undefined>;
+    css: string;
+    rules: Rule[];
+  }
+
+  export function renderStatic(fn: () => string | undefined): Result;
+  export function renderStaticOptimized(fn: () => string | undefined): Result;
 }
