40607910b0d1a559ee96a16138c422410ea3aafc
Marvin Hagemeister <marvin@marvinhagemeister.de>
2017-06-29 21:27:44 +0200

    Work on main export typings

--- styled.d.ts
+++ styled.d.ts
@@ -1,14 +1,48 @@
-declare module "emotion/styled" {
-  import { Component } from "react";
+declare module "emotion" {
+  export type InputValue = string | number;
 
-  export type Styler = (
+  export function flush(): void;
+  export function css(
+    cls: string,
     content: TemplateStringsArray,
-    vars?: string[] | number[],
-  ) => Component;
+    ...vars: InputValue[],
+  ): string;
 
-  export default function(tag: Component): Styler;
+  export function fragment(
+    content: TemplateStringsArray,
+    ...vars: InputValue[],
+  ): string;
+
+  export function keyframes(
+    kfm: string,
+    content: TemplateStringsArray,
+    ...vars: InputValue[],
+  ): string;
+
+  export function fontFace(
+    fontRules: string,
+    content: TemplateStringsArray,
+    ...vars: InputValue[],
+  ): string;
+
+  export function hydrate(ids: string[]): void;
+}
+
+declare module "emotion/styled" {
+  import { ComponentClass, StatelessComponent } from "react";
+
+  type Component<P> = ComponentClass<P> | StatelessComponent<P>;
+
+  export type InterpolationValue = string | number;
+
+  export type Tagged = (
+    content: TemplateStringsArray,
+    ...vars: InterpolationValue[],
+  ) => Component<any>;
+
+  export default function(tag: Component<any>): Tagged;
   export default function(
     tag: keyof HTMLElementTagNameMap,
     cls?: string,
-  ): Styler;
+  ): Tagged;
 }
