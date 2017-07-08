771ec0e064ab7bf75af269f61a96e579f156340a
Khoa Nguyen <thangngoc89@users.noreply.github.com>
2017-07-08 13:19:57 +0700

    Optimize omit utils

--- src/utils.js
+++ src/utils.js
@@ -3,9 +3,9 @@ export function omit (
   obj: { [string]: any },
   keys: Array<string>
 ) {
-  let target: { [string]: any } = {}
-  for (let i in obj) {
-    if (keys.indexOf(i) >= 0) continue
+  let target: { [string]: any } = {}, i: string
+  for (i in obj) {
+    if (keys.indexOf(i) !== -1) continue
     if (!Object.prototype.hasOwnProperty.call(obj, i)) continue
     target[i] = obj[i]
   }
