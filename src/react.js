b0aba8641497826aa7549db7309bf2bc70140fd6
Kye Hohenberger <kye@sideway.com>
2017-07-07 22:11:57 -0600

    Not tested yet

--- src/react.js
+++ src/react.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { css } from './index'
+import { Component, createElement as h } from 'react'
+import PropTypes from 'prop-types'
 import { createTheming } from 'theming'
+import { css } from './index'
+import { omit } from './utils'
 
 const theming = createTheming('__emotion__')
-
-const { channel, withTheme, ThemeProvider } = theming
-
-export { channel, withTheme, ThemeProvider }
+const { channel: CHANNEL, withTheme, ThemeProvider } = theming
+export { CHANNEL, withTheme, ThemeProvider }
 
 export default function (tag, cls, vars = [], content) {
   if (!tag) {
@@ -15,19 +15,53 @@ export default function (tag, cls, vars = [], content) {
     )
   }
 
-  function Styled (props) {
-    const getValue = v => (v && typeof v === 'function' ? v.cls || v(props) : v)
-    const className = css(cls.map(getValue), vars.map(getValue), content)
+  class Styled extends Component {
+    componentWillMount () {
+      if (this.context[CHANNEL]) {
+        this.setState({ theme: this.context[CHANNEL].getState() })
+      }
+    }
 
-    return React.createElement(
-      tag,
-      Object.assign({}, props, {
-        ref: props.innerRef,
-        className: props.className
-          ? className + ' ' + props.className
-          : className
-      })
-    )
+    componentDidMount () {
+      if (this.context[CHANNEL]) {
+        this.unsubscribe = this.context[CHANNEL].subscribe(this.setTheme)
+      }
+    }
+
+    componentWillUnmount () {
+      if (typeof this.unsubscribe === 'function') {
+        this.unsubscribe()
+      }
+    }
+
+    render () {
+      const { props, state } = this
+      const mergedProps = {
+        ...props,
+        theme: state.theme
+      }
+
+      const getValue = v =>
+        v && typeof v === 'function' ? v.cls || v(mergedProps) : v
+
+      const className = css(cls.map(getValue), vars.map(getValue), content)
+
+      return h(
+        tag,
+        Object.assign({}, omit(mergedProps, ['innerRef']), {
+          ref: mergedProps.innerRef,
+          className: mergedProps.className
+            ? className + ' ' + mergedProps.className
+            : className
+        })
+      )
+    }
+
+    setTheme = theme => this.setState({theme});
+  }
+
+  Styled.contextTypes = {
+    [CHANNEL]: PropTypes.object
   }
 
   const name = typeof cls[0] === 'string' ? cls[0].split('-')[1] : ''
