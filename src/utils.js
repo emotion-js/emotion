728338b00565efe534b53fdde0861f30106732b1
Khoa Nguyen <thangngoc89@users.noreply.github.com>
2017-07-08 13:31:28 +0700

    Split declaration

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,8 @@ export function omit (
   obj: { [string]: any },
   keys: Array<string>
 ) {
-  let target: { [string]: any } = {}, i: string
+  let target: { [string]: any } = {}
+  let i: string
   for (i in obj) {
     if (keys.indexOf(i) !== -1) continue
     if (!Object.prototype.hasOwnProperty.call(obj, i)) continue
