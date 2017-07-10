dd2b00c3dfd37879459e5c6d04bf81bb45b35678
Luke Edwards <luke.edwards05@gmail.com>
2017-07-10 15:31:07 -0700

    critical: hoist main regex

--- src/server.js
+++ src/server.js
@@ -1,21 +1,20 @@
 import { sheet, inserted } from './index'
 
+const RGX = /css(?:[a-zA-Z0-9-]*)-([a-zA-Z0-9]+)/gm
+
 export function extractCritical (html) {
   // parse out ids from html
   // reconstruct css/rules/cache to pass
   let o = { html, ids: [], css: '', rules: [] }
-  let regex = /css(?:[a-zA-Z0-9-]*)-([a-zA-Z0-9]+)/gm
   let match
   let ids = {}
-  while ((match = regex.exec(html)) !== null) {
+  while ((match = RGX.exec(html)) !== null) {
     if (!ids[match[1] + '']) {
       ids[match[1] + ''] = true
     }
   }
 
   o.rules = sheet.sheet.cssRules.filter(x => {
-    let regex = /css(?:[a-zA-Z0-9-]*)-([a-zA-Z0-9]+)/gm
-    let match = regex.exec(x.cssText)
     if (match && ids[match[1] + '']) {
       return true
     }
@@ -23,6 +22,7 @@ export function extractCritical (html) {
       return true
     }
     return false
+    let match = RGX.exec(x.cssText)
   })
   o.ids = Object.keys(inserted).filter(id => !!ids[id + ''])
   o.css = o.rules.map(x => x.cssText).join('')
