db3defcc85d425eb3e2b77b5bab87e5bcdd944b9
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-07-26 20:35:40 +1000

    Fix casing in macro react test

--- test/macro/react.test.js
+++ test/macro/react.test.js
@@ -145,9 +145,9 @@ describe('styled', () => {
   test('composes with objects', () => {
     const cssA = {
       color: lighten(0.2, '#000'),
-      'font-size': modularScale(1),
+      fontSize: modularScale(1),
       [hiDPI(1.5).replace('\n', ' ').trim()]: {
-        'font-size': modularScale(1.25)
+        fontSize: modularScale(1.25)
       }
     }
 
@@ -157,7 +157,7 @@ describe('styled', () => {
     `
 
     const H1 = styled('h1')`
-      composes: ${cssB}
+      composes: ${cssB};
       font-size: ${modularScale(4)};
     `
 
