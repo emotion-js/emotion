9a2be66507b57803f115884931e1fc9c0202e059
Kye Hohenberger <kye@sideway.com>
2017-07-26 22:52:52 -0600

    Clear out the lastIndex on loop iteration.

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ export function extractCritical (html) {
   // parse out ids from html
   // reconstruct css/rules/cache to pass
   const RGX = /css(?:[a-zA-Z0-9-]*)-([a-zA-Z0-9]+)/gm
-  
+
   let o = { html, ids: [], css: '', rules: [] }
   let match
   let ids = {}
@@ -27,7 +27,9 @@ export function extractCritical (html) {
 
   o.rules = sheet.sheet.cssRules.slice().filter(x => {
     let match = RGX.exec(x.cssText)
-    return match == null || ids[match[1]] || false
+    const ret = match == null || ids[match[1]] || false
+    RGX.lastIndex = 0
+    return ret
   })
 
   o.ids = keys(inserted).filter(id => !!ids[id])
