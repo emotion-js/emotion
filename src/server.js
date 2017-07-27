ec2ac67e60320ad5c1a6c603eedc4bb362e72247
Sunil Pai <threepointone@gmail.com>
2017-07-27 05:57:58 +0100

    fresh regex every time extractCritical is called (#177)
    * fresh regex every time extractCritical is called
    
    * Clear out the lastIndex on loop iteration.

--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 import { sheet, inserted } from './index'
 import { keys, forEach } from './utils'
 
-const RGX = /css(?:[a-zA-Z0-9-]*)-([a-zA-Z0-9]+)/gm
-
 export {
   flush,
   css,
@@ -16,6 +14,8 @@ export {
 export function extractCritical (html) {
   // parse out ids from html
   // reconstruct css/rules/cache to pass
+  const RGX = /css(?:[a-zA-Z0-9-]*)-([a-zA-Z0-9]+)/gm
+
   let o = { html, ids: [], css: '', rules: [] }
   let match
   let ids = {}
@@ -27,7 +27,9 @@ export function extractCritical (html) {
 
   o.rules = sheet.sheet.cssRules.slice().filter(x => {
     let match = RGX.exec(x.cssText)
-    return match == null || ids[match[1]] || false
+    const ret = match == null || ids[match[1]] || false
+    RGX.lastIndex = 0
+    return ret
   })
 
   o.ids = keys(inserted).filter(id => !!ids[id])
