32099c14aa9309961e529b7fdd5bbbd880b3985c
Kye Hohenberger <kye@sideway.com>
2017-08-02 11:51:27 -0600

    #212 - remove theming
    - first pass

--- src/react/index.js
+++ src/react/index.js
@@ -1,8 +1,6 @@
-import { Component, createElement as h } from 'react'
-import PropTypes from 'prop-types'
+import { createElement as h } from 'react'
 import { css } from '../index'
 import { map, omit, reduce, assign } from '../utils'
-import { CHANNEL } from './constants'
 
 export {
   flush,
@@ -25,98 +23,63 @@ export default function (tag, cls, objs, vars = [], content) {
 
   const componentTag = tag.displayName || tag.name || 'Component'
 
-  class Styled extends Component {
-    state = {
-      theme: {}
-    }
-
-    componentWillMount () {
-      if (this.context[CHANNEL]) {
-        this.setState({ theme: this.context[CHANNEL].getState() })
-      }
-    }
-
-    componentDidMount () {
-      if (this.context[CHANNEL]) {
-        this.unsubscribe = this.context[CHANNEL].subscribe(this.setTheme)
-      }
-    }
-
-    componentWillUnmount () {
-      if (typeof this.unsubscribe === 'function') {
-        this.unsubscribe()
-      }
-    }
-
-    render () {
-      const { props, state, context } = this
-      const mergedProps = assign({}, props, {
-        theme: state.theme
-      })
-
-      const getValue = v => {
-        if (v && typeof v === 'function') {
-          if (v.__emotion_class) {
-            return `& .${v.__emotion_class}`
-          }
-          return v(mergedProps, context)
+  function Styled (props, context) {
+    const getValue = v => {
+      if (v && typeof v === 'function') {
+        if (v.__emotion_class) {
+          return `& .${v.__emotion_class}`
         }
-
-        return v
-      }
-      let localTag = tag
-
-      let finalObjs = []
-
-      if (tag.__emotion_spec) {
-        push(
-          finalObjs,
-          reduce(
-            tag.__emotion_spec,
-            (accum, spec) => {
-              push(accum, spec.objs)
-              if (spec.content) {
-                push(accum, spec.content.apply(null, map(spec.vars, getValue)))
-              }
-              return accum
-            },
-            []
-          )
-        )
-        localTag = tag.__emotion_spec[0].tag
+        return v(props, context)
       }
 
-      push(finalObjs, objs)
+      return v
+    }
+    let localTag = tag
 
-      push(finalObjs, [cls])
+    let finalObjs = []
 
-      if (content) {
-        push(finalObjs, content.apply(null, map(vars, getValue)))
-      }
-
-      if (mergedProps.className) {
-        push(finalObjs, mergedProps.className.split(' '))
-      }
-
-      const className = css(map(finalObjs, getValue))
-
-      return h(
-        localTag,
-        omit(
-          assign({}, mergedProps, {
-            ref: mergedProps.innerRef,
-            className
-          }),
-          ['innerRef', 'theme']
+    if (tag.__emotion_spec) {
+      push(
+        finalObjs,
+        reduce(
+          tag.__emotion_spec,
+          (accum, spec) => {
+            push(accum, spec.objs)
+            if (spec.content) {
+              push(accum, spec.content.apply(null, map(spec.vars, getValue)))
+            }
+            return accum
+          },
+          []
         )
       )
+      localTag = tag.__emotion_spec[0].tag
     }
 
-    setTheme = theme => this.setState({ theme })
-  }
+    push(finalObjs, objs)
 
-  Styled.contextTypes = {
-    [CHANNEL]: PropTypes.object
+    push(finalObjs, [cls])
+
+    if (content) {
+      push(finalObjs, content.apply(null, map(vars, getValue)))
+    }
+
+    if (props.className) {
+      push(finalObjs, props.className.split(' '))
+    }
+
+    const className = css(map(finalObjs, getValue))
+
+    return h(
+      localTag,
+      omit(
+        assign({}, props, {
+          ref: props.innerRef,
+          className
+        }),
+        ['innerRef', 'theme']
+      )
+    )
   }
 
   Styled.displayName = `styled(${componentTag})`
