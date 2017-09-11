cb839b6a8466d04f4c41a848a5983026b596d7bf
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-09-11 21:15:13 +1000

    I was wrong

--- packages/emotion/src/index.js
+++ packages/emotion/src/index.js
@@ -122,9 +122,7 @@ function isLastCharDot(string) {
 function createStyles(strings, ...interpolations) {
   let stringMode = true
   let styles = ''
-  // probably need a better way to see if something is a tagged template literal
-  // can't use .raw since it should work with the loose version
-  if (typeof strings[0] !== 'string' || registered[strings[0]] !== undefined) {
+  if (strings !== undefined && strings.raw === undefined) {
     stringMode = false
     styles = handleInterpolation(strings, false)
   } else {
