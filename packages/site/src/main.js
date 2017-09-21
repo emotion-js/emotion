262e7c291235ec03a66e60826de6ec6daf77e9a6
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-09-21 11:07:27 +1000

    Move site to packages/

--- packages/site/src/main.js
+++ packages/site/src/main.js
@@ -10,7 +10,7 @@ import styled, {
 } from 'emotion/react'
 import Markdown from './markdown'
 import Playground from './playground'
-import logoUrl from '../../emotion.png'
+import logoUrl from '../../../emotion.png'
 
 const introExample = require('./blocks/intro.example')
 const propsExample = require('./blocks/props.example')
@@ -125,27 +125,27 @@ class App extends React.Component {
             </div>
             <Playground codeText={introExample} scope={scope} />
 
-            <Markdown markdown={require('../../docs/install.md')} />
+            <Markdown markdown={require('../../../docs/install.md')} />
 
-            <Markdown markdown={require('../../docs/nested.md')} />
+            <Markdown markdown={require('../../../docs/nested.md')} />
             <Playground
               maxHeight={180}
               codeText={nestedExample}
               scope={scope}
             />
 
-            <Markdown markdown={require('../../docs/pseudo.md')} />
+            <Markdown markdown={require('../../../docs/pseudo.md')} />
             <Playground
               maxHeight={220}
               codeText={pseudoExample}
               scope={scope}
             />
 
-            <Markdown markdown={require('../../docs/media.md')} />
+            <Markdown markdown={require('../../../docs/media.md')} />
             <Playground maxHeight={220} codeText={mediaExample} scope={scope} />
 
             <Markdown
-              markdown={require('../../docs/styling-any-component.md')}
+              markdown={require('../../../docs/styling-any-component.md')}
             />
             <Playground
               maxHeight={220}
@@ -153,45 +153,47 @@ class App extends React.Component {
               scope={scope}
             />
 
-            <Markdown markdown={require('../../docs/props.md')} />
+            <Markdown markdown={require('../../../docs/props.md')} />
             <Playground codeText={propsExample} scope={scope} />
 
-            <Markdown markdown={require('../../docs/keyframes.md')} />
+            <Markdown markdown={require('../../../docs/keyframes.md')} />
             <Playground
               maxHeight={600}
               codeText={keyframesExample}
               scope={scope}
             />
 
-            <Markdown markdown={require('../../docs/composition.md')} />
+            <Markdown markdown={require('../../../docs/composition.md')} />
             <Playground
               maxHeight={180}
               codeText={require('./blocks/composition.example')}
               scope={scope}
             />
 
-            <Markdown markdown={require('../../docs/objects.md')} />
+            <Markdown markdown={require('../../../docs/objects.md')} />
             <Playground
               maxHeight={180}
               codeText={require('./blocks/objects.example')}
               scope={scope}
             />
 
-            <Markdown markdown={require('../../docs/styled-with-object.md')} />
+            <Markdown
+              markdown={require('../../../docs/styled-with-object.md')}
+            />
             <Playground
               maxHeight={180}
               codeText={require('./blocks/styled-with-object.example')}
               scope={scope}
             />
 
-            <Markdown markdown={require('../../docs/css-prop.md')} />
+            <Markdown markdown={require('../../../docs/css-prop.md')} />
             <Playground
               maxHeight={180}
               codeText={require('./blocks/css-prop.example')}
               scope={scope}
             />
 
-            <Markdown markdown={require('../../docs/theming.md')} />
+            <Markdown markdown={require('../../../docs/theming.md')} />
             <Playground
               maxHeight={180}
               codeText={require('./blocks/theming.example')}
