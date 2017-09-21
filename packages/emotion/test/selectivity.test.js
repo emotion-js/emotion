fd775e7784197cab67633dcba0331928fcd3176a
Kye Hohenberger <kye@sideway.com>
2017-09-21 16:20:44 -0600

    Update tests for selectivity to include nested rules

--- packages/emotion/test/selectivity.test.js
+++ packages/emotion/test/selectivity.test.js
@@ -3,6 +3,38 @@ import renderer from 'react-test-renderer'
 import { css, sheet } from 'emotion'
 
 describe('css', () => {
+  afterEach(() => {
+    sheet.flush()
+    sheet.inject()
+  })
+
+  test('complex nested styles', () => {
+    const mq = [
+      '@media(min-width: 420px)',
+      '@media(min-width: 640px)',
+      '@media(min-width: 960px)'
+    ]
+
+    const cls1 = css({
+      color: 'blue',
+      '&:hover': {
+        '& .name': {
+          color: 'amethyst',
+          '&:focus': {
+            color: 'burlywood',
+            [mq[0]]: {
+              color: 'rebeccapurple'
+            }
+          }
+        },
+        color: 'green'
+      }
+    })
+    const tree = renderer.create(<div className={cls1} />).toJSON()
+    expect(tree).toMatchSnapshot()
+    expect(sheet).toMatchSnapshot()
+  })
+
   test('handles media query merges', () => {
     const mq = [
       '@media(min-width: 420px)',
@@ -39,7 +71,7 @@ describe('css', () => {
       },
       buttonCSS
     ])
-    const tree = renderer.create(<div className={cls1}/>).toJSON()
+    const tree = renderer.create(<div className={cls1} />).toJSON()
     expect(tree).toMatchSnapshot()
     expect(sheet).toMatchSnapshot()
   })
