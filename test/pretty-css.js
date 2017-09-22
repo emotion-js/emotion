dd4d84a7ac9e84a162a5fe675a81f4bbe5bfad19
Kye Hohenberger <kye@sideway.com>
2017-09-21 22:02:51 -0600

    Revert pretty css changes

--- test/pretty-css.js
+++ test/pretty-css.js
@@ -5,16 +5,7 @@ export default {
   test: val => val === sheet,
   print(val, printer) {
     return printer(
-      stringify(
-        parse(
-          sheet.tags
-            .map(
-              tag =>
-                `${tag.getAttribute('data-emotion')}\n${tag.textContent || ''}`
-            )
-            .join('')
-        )
-      )
+      stringify(parse(sheet.tags.map(tag => tag.textContent || '').join('')))
     )
   }
 }
