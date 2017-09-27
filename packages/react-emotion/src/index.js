aff90295032a653cf4187e23765fa13478a7a359
Kye Hohenberger <kye@sideway.com>
2017-09-27 16:22:03 -0600

    Attach theming CHANNEL updates to styled components.

--- packages/react-emotion/src/index.js
+++ packages/react-emotion/src/index.js
@@ -1,7 +1,9 @@
 /* global codegen */
-import { createElement } from 'react'
+import { createElement, Component } from 'react'
 import { memoize } from 'emotion-utils'
+import PropTypes from 'prop-types'
 import { css, getRegisteredStyles } from 'emotion'
+import { channel as CHANNEL } from 'emotion-theming'
 
 export * from 'emotion'
 
@@ -57,35 +59,69 @@ const createStyled = (tag, options: { e: string }) => {
       }
     }
 
-    const Styled = (props, context) => {
-      const getValue = v => {
-        if (typeof v === 'function') {
-          return v(props, context)
+    class Styled extends Component {
+      componentWillMount() {
+        if (this.context[CHANNEL]) {
+          this.setState({ theme: this.context[CHANNEL].getState() })
         }
-        return v
       }
 
-      let className = ''
-      let classInterpolations = []
+      componentDidMount() {
+        if (this.context[CHANNEL]) {
+          this.unsubscribe = this.context[CHANNEL].subscribe(this.setTheme)
+        }
+      }
 
-      if (props.className) {
+      componentWillUnmount() {
+        if (typeof this.unsubscribe === 'function') {
+          this.unsubscribe()
+        }
+      }
+      setTheme = theme => this.setState({ theme })
+
+      render() {
+        const { props, state, context } = this
+        let mergedProps = props
+        if (state && state.theme) {
+          mergedProps = {
+            ...props,
+            theme: state.theme || {}
+          }
+        }
+
+        const getValue = v => {
+          if (typeof v === 'function') {
+            return v(mergedProps, context)
+          }
+          return v
+        }
+
+        let className = ''
+        let classInterpolations = []
+
+        if (props.className) {
+          if (staticClassName === false) {
+            className += getRegisteredStyles(
+              classInterpolations,
+              props.className
+            )
+          } else {
+            className += `${props.className} `
+          }
+        }
         if (staticClassName === false) {
-          className += getRegisteredStyles(classInterpolations, props.className)
+          className += css(...styles.map(getValue), ...classInterpolations)
         } else {
-          className += `${props.className} `
+          className += staticClassName
         }
-      }
-      if (staticClassName === false) {
-        className += css(...styles.map(getValue), ...classInterpolations)
-      } else {
-        className += staticClassName
-      }
 
-      return createElement(
-        baseTag,
-        omitAssign(omitFn, {}, props, { className, ref: props.innerRef })
-      )
+        return createElement(
+          baseTag,
+          omitAssign(omitFn, {}, props, { className, ref: props.innerRef })
+        )
+      }
     }
+    Styled.contextTypes = { [CHANNEL]: PropTypes.object }
 
     Styled.__emotion_styles = styles
     Styled.__emotion_base = baseTag
