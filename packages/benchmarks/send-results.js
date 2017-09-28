b44540d3c03eb1d67a8719d2f60404d0facf2330
Kye Hohenberger <kye@sideway.com>
2017-09-27 23:53:44 -0600

    Hook benchmarks to db (#353)
    * Hook benchmarks to db
    
    * Format
    
    * Format
    
    * Format

---
+++ packages/benchmarks/send-results.js
@@ -0,0 +1,38 @@
+const { createApolloFetch } = require('apollo-fetch')
+
+const { GRAPH_TOKEN } = process.env
+
+const client = createApolloFetch({
+  uri: 'https://api.graph.cool/simple/v1/cj83urcdm0u420180bqw9w4mu'
+})
+
+client.use(({ request, options }, next) => {
+  if (!options.headers) {
+    options.headers = {} // Create the headers object if needed.
+  }
+  options.headers['Authorization'] = 'Bearer ' + GRAPH_TOKEN || ''
+
+  next()
+})
+
+module.exports = function sendResult(vars) {
+  return client({
+    query: `
+      mutation createRunAndResults($branch: String, $pr: String, $commit: String, $commitMessage: String, $results: [RunresultsResult!]) {
+        createRun(results: $results, branch: $branch, pr: $pr, commit: $commit, commitMessage: $commitMessage) {
+          createdAt
+          branch
+          pr
+          commit
+          commitMessage
+          results {
+            name
+            duration
+            type
+          }
+        }
+      }
+    `,
+    variables: vars
+  })
+}
