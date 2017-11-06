e08cba784c64d3c87094cec7f371005bdbff61eb
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-11-06 22:08:28 +1000

    Add deps and more stuff to extractCritical

--- packages/emotion-server/src/index.js
+++ packages/emotion-server/src/index.js
@@ -7,21 +7,21 @@ export function extractCritical(html) {
   // reconstruct css/rules/cache to pass
   const RGX = /css-([a-zA-Z0-9]+)/gm
 
-  let o = { html, ids: [], css: '', rules: [] }
+  let o = { html, ids: [], css: '' }
   let match
   let ids = {}
   while ((match = RGX.exec(html)) !== null) {
-    if (!ids[match[1]]) {
+    if (ids[match[1]] === undefined) {
       ids[match[1]] = true
     }
   }
 
   o.ids = Object.keys(inserted).filter(id => {
-    return ids[id] === true || registered[`css-${id}`] === undefined
+    if (ids[id] === true || registered[`css-${id}`] === undefined) {
+      o.css += inserted[id]
+      return true
+    }
   })
-
-  o.rules = o.ids.map(key => inserted[key])
-  o.css = o.rules.join('')
 
   return o
 }
