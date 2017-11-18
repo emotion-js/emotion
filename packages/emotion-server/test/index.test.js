2c979e543ba90482584e3160ff176e026ad46e23
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-11-18 12:02:15 +1000

    Fix some tests

--- packages/emotion-server/test/index.test.js
+++ packages/emotion-server/test/index.test.js
@@ -3,31 +3,40 @@
 */
 import React from 'react'
 import { renderToString } from 'react-dom/server'
-import { extractCritical } from 'emotion-server'
 import { getComponents, prettyifyCritical } from './util'
 
 let emotion = require('emotion')
 let reactEmotion = require('react-emotion')
+let emotionServer = require('emotion-server')
 
 describe('extractCritical', () => {
   test('returns static css', () => {
     const { Page1, Page2 } = getComponents(emotion, reactEmotion)
     expect(
-      prettyifyCritical(extractCritical(renderToString(<Page1 />)))
+      prettyifyCritical(
+        emotionServer.extractCritical(renderToString(<Page1 />))
+      )
     ).toMatchSnapshot()
     expect(
-      prettyifyCritical(extractCritical(renderToString(<Page2 />)))
+      prettyifyCritical(
+        emotionServer.extractCritical(renderToString(<Page2 />))
+      )
     ).toMatchSnapshot()
   })
 })
 describe('hydration', () => {
   test('only rules that are not in the critical css are inserted', () => {
     const { Page1 } = getComponents(emotion, reactEmotion)
-    const { html, ids, css } = extractCritical(renderToString(<Page1 />))
+    const { html, ids, css } = emotionServer.extractCritical(
+      renderToString(<Page1 />)
+    )
     expect(prettyifyCritical({ html, css, ids })).toMatchSnapshot()
     jest.resetModules()
+    global.__SECRET_EMOTION__ = undefined
     emotion = require('emotion')
+    emotionServer = require('emotion-server')
     expect(emotion.inserted).toEqual({})
+    expect(emotion.inserted).toBe(global.__SECRET_EMOTION__)
     emotion.hydrate(ids)
     const { Page1: NewPage1 } = getComponents(emotion, reactEmotion)
     renderToString(<NewPage1 />)
