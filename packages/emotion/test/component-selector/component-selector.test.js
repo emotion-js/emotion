bc855c1d98c086f2ff0d7a764bc24e20e9ad8df5
Evan Scott <probablyup@gmail.com>
2017-11-25 18:43:56 -0500

    coverage

--- packages/emotion/test/component-selector/component-selector.test.js
+++ packages/emotion/test/component-selector/component-selector.test.js
@@ -2,9 +2,11 @@ import React from 'react'
 import styled from 'react-emotion'
 import renderer from 'react-test-renderer'
 import { css, flush, sheet } from 'emotion'
+import { TARGET_KEY } from 'emotion-utils'
 
 describe('component selector', () => {
   afterEach(() => flush())
+
   test('should be converted to use the emotion target className', () => {
     const FakeComponent = styled.div`
       color: blue;
@@ -24,5 +26,21 @@ describe('component selector', () => {
       .toJSON()
     expect(tree).toMatchSnapshot()
     expect(sheet).toMatchSnapshot()
+  })
+
+  test('should throw if the missing the static targeting property', () => {
+    const FakeComponent = styled.div`
+      color: blue;
+    `
+
+    delete FakeComponent[TARGET_KEY]
+
+    expect(() => {
+      css`
+        ${FakeComponent} {
+          color: red;
+        }
+      `
+    }).toThrow()
   })
 })
