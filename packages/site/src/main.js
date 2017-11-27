476bff9c2527e61a343a67769670eac20822c754
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-11-28 09:33:55 +1000

    Remove fontFace docs

--- packages/site/src/main.js
+++ packages/site/src/main.js
@@ -1,13 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { ThemeProvider, withTheme } from 'emotion-theming'
-import styled, {
-  css,
-  fontFace,
-  keyframes,
-  injectGlobal,
-  merge
-} from 'react-emotion'
+import styled, { css, keyframes, injectGlobal, merge } from 'react-emotion'
 import Markdown from './markdown'
 import Playground from './playground'
 import logoUrl from '../../../emotion.png'
@@ -48,14 +42,13 @@ injectGlobal`
     padding: 0;
     margin: 0;
   }
-`
-
-fontFace`
-  font-family: 'Oxygen';
-  font-style: normal;
-  font-weight: 400;
-  src: local('Oxygen Regular'), local('Oxygen-Regular'), url(https://fonts.gstatic.com/s/oxygen/v6/qBSyz106i5ud7wkBU-FrPevvDin1pK8aKteLpeZ5c0A.woff2) format('woff2');
-  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215;
+  @font-face {
+    font-family: 'Oxygen';
+    font-style: normal;
+    font-weight: 400;
+    src: local('Oxygen Regular'), local('Oxygen-Regular'), url(https://fonts.gstatic.com/s/oxygen/v6/qBSyz106i5ud7wkBU-FrPevvDin1pK8aKteLpeZ5c0A.woff2) format('woff2');
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215;
+  }
 `
 
 const theme = {
