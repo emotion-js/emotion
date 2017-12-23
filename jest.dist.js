7746fa6c352326a1c8850d925a2d6d9832601246
Mateusz Burzyński <mateuszburzynski@gmail.com>
2017-12-23 23:30:35 +0100

    Added lerna-alias dep to automate creating jest aliases (#506)

---
+++ jest.dist.js
@@ -0,0 +1,8 @@
+const { jest: lernaAliases } = require('lerna-alias')
+const baseConfig = require('./jest.config.js')
+
+module.exports = Object.assign({}, baseConfig, {
+  moduleNameMapper: lernaAliases({ sourceDirectory: false }),
+  transformIgnorePatterns: ['dist', 'node_modules'],
+  testPathIgnorePatterns: ['babel-plugin-emotion']
+})
