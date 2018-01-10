be37eab55dd75eb717986b8adf4cbc157ca87139
mitchellhamilton <mitchell@mitchellhamilton.me>
2018-01-11 08:10:00 +1000

    Fix z-index

--- packages/site/src/components/Search.js
+++ packages/site/src/components/Search.js
@@ -42,7 +42,7 @@ class Search extends React.Component<Props, State> {
 
   render() {
     return this.state.enabled ? (
-      <form>
+      <form css={{ zIndex: 300 }}>
         <input
           ref={ele => {
             this.input = ele
