c3e05212265df32942938a50ba1f143c6e106efb
Junyoung Clare Jang <jjc9310@gmail.com>
2018-05-24 03:19:55 -0400

    Divide typings into two files

---
+++ packages/create-emotion-styled/types/react.d.ts
@@ -0,0 +1,79 @@
+// Definitions by: Junyoung Clare Jang <https://github.com/Ailrun>
+// TypeScript Version: 2.3
+
+import React, { ComponentClass, ReactHTML, ReactSVG, Ref, SFC, StatelessComponent } from 'react';
+
+import {
+  Interpolation,
+  StyledOptions,
+  StyledOtherProps,
+  StyledStatelessProps,
+  Themed,
+} from './common';
+
+export interface StyledComponentMethods<Props extends object, InnerProps extends object, Theme extends object> {
+  withComponent<T extends keyof ReactHTML>(
+    tag: T,
+    options?: StyledOptions,
+  ): StyledOtherComponent<Props, ReactHTML[T], Theme>;
+
+  withComponent<T extends keyof ReactSVG>(
+    tag: T,
+    options?: StyledOptions,
+  ): StyledOtherComponent<Props, ReactSVG[T], Theme>;
+
+  withComponent<IP extends object>(
+    component: SFC<IP>,
+    options?: StyledOptions,
+  ): StyledStatelessComponent<Props, IP, Theme>;
+
+  withComponent<IP extends object>(
+    component: ComponentClass<IP>,
+    options?: StyledOptions,
+  ): StyledOtherComponent<Props, IP, Theme>;
+}
+
+export interface StyledStatelessComponent<Props extends object, InnerProps extends object, Theme extends object>
+  extends ComponentClass<StyledStatelessProps<Props & InnerProps, Theme>>,
+    StyledComponentMethods<Props, InnerProps, Theme> {}
+
+export interface StyledOtherComponent<Props extends object, InnerProps extends object, Theme extends object>
+  extends ComponentClass<StyledOtherProps<Props & InnerProps, Theme, Ref<any>>>,
+    StyledComponentMethods<Props, InnerProps, Theme> {}
+
+export type StyledComponent<Props extends object, InnerProps extends object, Theme extends object> =
+  | StyledStatelessComponent<Props, InnerProps, Theme>
+  | StyledOtherComponent<Props, InnerProps, Theme>
+  ;
+
+export type CreateStyledStatelessComponent<InnerProps extends object, Theme extends object> =
+  <Props extends object, OverridedTheme extends object = Theme>(
+    ...args: Array<Interpolation<Themed<Props, OverridedTheme>>>
+  ) => StyledStatelessComponent<Props, InnerProps, OverridedTheme>;
+
+export type CreateStyledOtherComponent<InnerProps extends object, Theme extends object> =
+  <Props extends object, OverridedTheme extends object = Theme>(
+    ...args: Array<Interpolation<Themed<Props, OverridedTheme>>>
+  ) => StyledOtherComponent<Props, InnerProps, OverridedTheme>;
+
+export interface CreateStyled<Theme extends object = any> {
+  <T extends keyof ReactHTML>(
+    tag: T,
+    options?: StyledOptions,
+  ): CreateStyledOtherComponent<ReactHTML[T], Theme>;
+
+  <T extends keyof ReactSVG>(
+    tag: T,
+    options?: StyledOptions,
+  ): CreateStyledOtherComponent<ReactSVG[T], Theme>;
+
+  <IP extends object>(
+    component: SFC<IP>,
+    options?: StyledOptions,
+  ): CreateStyledStatelessComponent<IP, Theme>;
+
+  <IP extends object>(
+    component: ComponentClass<IP>,
+    options?: StyledOptions,
+  ): CreateStyledOtherComponent<IP, Theme>;
+}
