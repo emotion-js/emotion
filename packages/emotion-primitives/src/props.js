801c69df7e62514b6461a56ea4498200e808292c
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-06-14 16:44:50 +0200

    Optimized isValidStyleProp implementation a little bit

--- packages/emotion-primitives/src/props.js
+++ packages/emotion-primitives/src/props.js
@@ -2,21 +2,17 @@ import viewStylePropTypes from 'react-primitives/lib/web/View/ViewStylePropTypes
 import textStylePropTypes from 'react-primitives/lib/web/Text/TextStylePropTypes'
 import imageStylePropTypes from 'react-primitives/lib/web/Image/ImageStylePropTypes'
 
-const viewStyleProps = Object.keys(viewStylePropTypes)
-const textStyleProps = Object.keys(textStylePropTypes)
-const imageStyleProps = Object.keys(imageStylePropTypes)
-
 export const isValidStyleProp = (element, propName) => {
-  if (element === 'Text') return textStyleProps.indexOf(propName) > -1
-  if (element === 'View') return viewStyleProps.indexOf(propName) > -1
-  if (element === 'Image') return imageStyleProps.indexOf(propName) > -1
+  if (element === 'Text') return textStylePropTypes[propName]
+  if (element === 'View') return viewStylePropTypes[propName]
+  if (element === 'Image') return imageStylePropTypes[propName]
 
   if (typeof element === 'function' && element.name === 'Text')
-    return textStyleProps.indexOf(propName) > -1
+    return textStylePropTypes[propName]
   if (typeof element === 'function' && element.name === 'View')
-    return viewStyleProps.indexOf(propName) > -1
+    return viewStylePropTypes[propName]
   if (typeof element === 'function' && element.name === 'Image')
-    return imageStyleProps.indexOf(propName) > -1
+    return imageStylePropTypes[propName]
 
   return false
 }
