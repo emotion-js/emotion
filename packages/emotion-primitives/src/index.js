be286206732a5365a58b6726c1e6c27c3b2d0d9b
Nitin <tulswani19@gmail.com>
2018-06-26 22:00:22 +0530

    remove proxy

--- packages/emotion-primitives/src/index.js
+++ packages/emotion-primitives/src/index.js
@@ -3,8 +3,6 @@ import reactPrimitives from 'react-primitives'
 import { createEmotionPrimitive } from './createEmotion'
 import { splitProps } from './splitProps'
 
-let validate
-
 const primitives = ['Text', 'View', 'Image']
 
 const assignPrimitives = styled => {
@@ -21,30 +19,4 @@ const assignPrimitives = styled => {
 
 const styled = createEmotionPrimitive(splitProps)
 
-if (process.env.NODE_ENV !== 'production' && typeof Proxy !== 'undefined') {
-  const Platform = reactPrimitives.Platform
-
-  // Validate primitives accessed using the emotion function directly like emotion.TEXT`` or emotion.VIEW``
-  // Proxy is not supported in Native or Sketch. So makes sure that errors are not logged out in those env.
-  if (Platform.OS === 'web') {
-    validate = target => {
-      const handler = {
-        get: (obj, prop) => {
-          if (prop in obj) {
-            return obj[prop]
-          } else {
-            throw new Error(
-              `Cannot style invalid primitive ${prop}. Expected primitive to be one of ['Text', 'View', 'Image']`
-            )
-          }
-        }
-      }
-
-      return new Proxy(target, handler)
-    }
-  } else {
-    validate = fn => fn
-  }
-}
-
-export default validate(assignPrimitives(styled))
+export default assignPrimitives(styled)
