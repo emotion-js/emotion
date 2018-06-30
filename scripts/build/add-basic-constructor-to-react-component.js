54acf9feb7f6b94a248139895b16f6edf749bcae
mitchellhamilton <mitchell@mitchellhamilton.me>
2018-06-30 15:39:18 +1000

    Refactor and add comment to react component constructor babel plugin

--- scripts/build/add-basic-constructor-to-react-component.js
+++ scripts/build/add-basic-constructor-to-react-component.js
@@ -1,37 +1,34 @@
 // this is to avoid the spread argument helper for react component constructors
+// https://runkit.com/mitchellhamilton/5b370eeaf5b34c0012874d92
+// without this, the constructor's arguments will be iterated over and put in an array
 module.exports = function(babel) {
   const { types: t } = babel
-
+  let isReactComponent = t.buildMatchMemberExpression('React.Component')
+  let hasConstructor = body =>
+    body.length &&
+    body.some(node => {
+      return (
+        t.isClassMethod(node) &&
+        t.isIdentifier(node.key) &&
+        node.key.name === 'constructor'
+      )
+    })
   return {
     name: 'ast-transform', // not required
     visitor: {
       Class(path) {
         if (
-          path.node.superClass &&
-          path.node.superClass.object &&
-          path.node.superClass.object.name === 'React' &&
-          path.node.superClass.property &&
-          path.node.superClass.property.name === 'Component' &&
-          path.node.body.body.length &&
-          !path.node.body.body.some(node => {
-            return (
-              t.isClassMethod(node) &&
-              t.isIdentifier(node.key) &&
-              node.key.name === 'constructor'
-            )
-          })
+          isReactComponent(path.node.superClass) &&
+          !hasConstructor(path.node.body.body)
         ) {
           path.node.body.body.unshift(
-            t.classMethod(
-              'constructor',
-              t.identifier('constructor'),
-              [t.identifier('props')],
-              t.blockStatement([
-                t.expressionStatement(
-                  t.callExpression(t.super(), [t.identifier('props')])
-                )
-              ])
-            )
+            babel.template`
+              class Thing {
+                constructor(props) {
+                  super(props);
+                }
+              }
+            `({}).body.body[0]
           )
         }
       }
