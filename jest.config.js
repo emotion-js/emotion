3a0fe42c2b4e3f3d73215075100d76f98d01979c
mitchellhamilton <mitchell@mitchellhamilton.me>
2018-07-01 18:38:26 +1000

    Add to coverage ignore

--- jest.config.js
+++ jest.config.js
@@ -10,5 +10,9 @@ module.exports = {
     'jest-watch-typeahead/testname'
   ],
   moduleNameMapper: lernaAliases(),
-  setupTestFrameworkScriptFile: '<rootDir>/test/testSetup.js'
+  setupTestFrameworkScriptFile: '<rootDir>/test/testSetup.js',
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/packages/babel-plugin-emotion/test/util.js'
+  ]
 }
