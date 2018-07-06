2cfe87a24c0a595619a231524820b7f484de3f1f
mitchellhamilton <mitchell@mitchellhamilton.me>
2018-07-06 15:19:55 +1000

    Change interleave implementation

--- packages/emotion-primitives/src/utils.js
+++ packages/emotion-primitives/src/utils.js
@@ -32,23 +32,14 @@ export const pickAssign: (
   return target
 }
 
-// export function interleave(vals: Array<*>) {
-//   let strings = vals[0]
-//   let finalArray = [strings[0]]
-//   for (let i = 1, len = vals.length; i < len; i++) {
-//     finalArray.push(vals[i])
-//     if (strings[i + 1] !== undefined) {
-//       finalArray.push(strings[i + 1])
-//     }
-//   }
-//   return finalArray
-// }
-
-export function interleave([strings, ...interpolations]: any) {
-  return interpolations.reduce(
-    function(array, interp, i) {
-      return array.concat([interp], strings[i + 1])
-    },
-    [strings[0]]
-  )
+export function interleave(vals: Array<*>) {
+  let strings = vals[0]
+  let finalArray = [strings[0]]
+  for (let i = 1, len = vals.length; i < len; i++) {
+    finalArray.push(vals[i])
+    if (strings[i] !== undefined) {
+      finalArray.push(strings[i])
+    }
+  }
+  return finalArray
 }
