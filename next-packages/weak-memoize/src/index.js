873ad6824b98038f7939a4351ef7e9675b2523a0
mitchellhamilton <mitchell@mitchellhamilton.me>
2018-07-24 18:35:49 +1000

    Add a flow comment

--- next-packages/weak-memoize/src/index.js
+++ next-packages/weak-memoize/src/index.js
@@ -1,7 +1,9 @@
+// @flow
 let weakMemoize = function<Arg, Return>(func: Arg => Return): Arg => Return {
   let cache: WeakMap<Arg, Return> = new WeakMap()
   return arg => {
     if (cache.has(arg)) {
+      // $FlowFixMe
       return cache.get(arg)
     }
     let ret = func(arg)
