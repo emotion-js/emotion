d7b40fdcb45e830cbc946eb9b5fac8880f36895c
mitchellhamilton <mitchell@mitchellhamilton.me>
2018-07-26 18:42:48 +1000

    Remove unused thing

--- scripts/test-utils/legacy-env.js
+++ scripts/test-utils/legacy-env.js
@@ -5,8 +5,6 @@ import * as emotion from 'emotion'
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
-process.env.LEGACY_TEST = 'true'
-
 expect.addSnapshotSerializer(createSerializer(emotion))
 
 Enzyme.configure({ adapter: new Adapter() })
