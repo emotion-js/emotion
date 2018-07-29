ade3bad31f47adf6b64970c1859f00fc6ba1b7a7
mitchellhamilton <mitchell@mitchellhamilton.me>
2018-07-29 11:05:46 +1000

    Add a temp fix for something

--- scripts/build/rollup.config.js
+++ scripts/build/rollup.config.js
@@ -18,13 +18,31 @@ const makeExternalPredicate = externalArr => {
 
 let unsafeRequire = require
 
+let pkgJsonsAllowedToFail = [
+  // the package.json can't be found for this package on ci so for now,
+  // we're just going to ignore it
+  // TODO: investigate why it fails
+  'nopt'
+]
+
 function getChildPeerDeps(
   finalPeerDeps /*: Array<string> */,
   isUMD /*: boolean */,
   depKeys /*: Array<string> */
 ) {
   depKeys.forEach(key => {
-    const pkgJson = unsafeRequire(key + '/package.json')
+    let pkgJson
+    try {
+      pkgJson = unsafeRequire(key + '/package.json')
+    } catch (err) {
+      if (
+        err.code === 'MODULE_NOT_FOUND' &&
+        pkgJsonsAllowedToFail.includes(key)
+      ) {
+        return
+      }
+      throw err
+    }
     if (pkgJson.peerDependencies) {
       finalPeerDeps.push(...Object.keys(pkgJson.peerDependencies))
       getChildPeerDeps(
