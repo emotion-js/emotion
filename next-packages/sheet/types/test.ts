abe639d1ad2d23091dcf23dcaeeacf4337859fb6
mitchellhamilton <mitchell@mitchellhamilton.me>
2018-08-02 22:48:56 +1000

    ts.............

--- next-packages/sheet/types/test.ts
+++ next-packages/sheet/types/test.ts
@@ -1,56 +1,59 @@
-import { Options, StyleSheet } from '@emotion/sheet';
+import { Options, StyleSheet } from '@emotion/sheet'
 
-new StyleSheet();
-new StyleSheet({});
 new StyleSheet({
-  container: document.body,
-});
+  key: 'abc',
+  container: document.body
+})
+new StyleSheet({
+  key: 'abc',
+  container: document.head
+})
+new StyleSheet({
+  key: 'abc',
+  container: document.head
+})
+
+new StyleSheet({
+  key: 'abc',
+  container: document.head,
+  nonce: 'fefwe090rqr'
+})
+new StyleSheet({
+  key: 'abc',
+  container: document.head,
+  speedy: true
+})
+// $ExpectError
 new StyleSheet({
   container: document.head,
-});
+  key: 120
+})
 new StyleSheet({
-  key: 'abc',
-});
-new StyleSheet({
-  key: 'abc',
-  maxLength: 200,
-});
-new StyleSheet({
-  nonce: 'fefwe090rqr',
-});
-new StyleSheet({
-  speedy: true,
-});
-// $ExpectError
-new StyleSheet({
-  maxLength: 'abc',
-});
-// $ExpectError
-new StyleSheet({
-  key: 120,
-});
-new StyleSheet({
+  container: document.head,
   // $ExpectError
   kye: 'abc'
-});
+})
 
-const styleSheet0 = new StyleSheet();
-const styleSheet1: StyleSheet = styleSheet0;
-const styleSheet2: StyleSheet = new StyleSheet();
+const styleSheet0 = new StyleSheet()
+const styleSheet1: StyleSheet = styleSheet0
+const styleSheet2: StyleSheet = new StyleSheet()
 
-const styleSheet = new StyleSheet();
+const styleSheet = new StyleSheet()
 
-styleSheet.insert('.name{ color: black; }');
-styleSheet.insert('.cl{ width: 200px; height: 200px; }');
+styleSheet.insert('.name{ color: black; }')
+styleSheet.insert('.cl{ width: 200px; height: 200px; }')
 // $ExpectError
-styleSheet.insert();
+styleSheet.insert()
 // $ExpectError
-styleSheet.insert('.name{ color: black; }', (undefined as any));
+styleSheet.insert('.name{ color: black; }', undefined as any)
 // $ExpectError
-styleSheet.insert('.name{ color: black; }', ...(undefined as any as Array<any>));
+styleSheet.insert(
+  '.name{ color: black; }',
+  ...((undefined as any) as Array<any>)
+)
 
-styleSheet.flush();
+styleSheet.flush()
 // $ExpectError
-styleSheet.flush((undefined as any));
+styleSheet.flush(undefined as any)
 // $ExpectError
-styleSheet.flush(...(undefined as any as Array<any>));
+styleSheet.flush(...((undefined as any) as Array<any>))
