ce37c79dbe19c692f135f7d72e8124f892a48711
mitchellhamilton <mitchell@mitchellhamilton.me>
2018-08-02 22:58:56 +1000

    maybe this'll fix it?

--- next-packages/sheet/types/test.ts
+++ next-packages/sheet/types/test.ts
@@ -2,40 +2,40 @@ import { Options, StyleSheet } from '@emotion/sheet'
 
 new StyleSheet({
   key: 'abc',
-  container: document.head
+  container: document.createElement('div')
 })
 
 new StyleSheet({
   key: 'abc',
-  container: document.head,
+  container: document.createElement('div'),
   nonce: 'fefwe090rqr'
 })
 new StyleSheet({
   key: 'abc',
-  container: document.head,
+  container: document.createElement('div'),
   speedy: true
 })
 // $ExpectError
 new StyleSheet({
-  container: document.head,
+  container: document.createElement('div'),
   key: 120
 })
 new StyleSheet({
-  container: document.head,
+  container: document.createElement('div'),
   // $ExpectError
   kye: 'abc'
 })
 
 const styleSheet0 = new StyleSheet({
   key: 'abc',
-  container: document.head
+  container: document.createElement('div')
 })
 const styleSheet1: StyleSheet = styleSheet0
 const styleSheet2: StyleSheet = new StyleSheet()
 
 const styleSheet = new StyleSheet({
   key: 'abc',
-  container: document.head
+  container: document.createElement('div')
 })
 
 styleSheet.insert('.name{ color: black; }')
