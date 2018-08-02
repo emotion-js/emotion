bdace92d153cb055ad4096cfcddd7bc6cfb6736d
mitchellhamilton <mitchell@mitchellhamilton.me>
2018-08-02 17:12:41 +1000

    Add a comment about something totally unrelated

--- next-packages/babel-utils/src/object-to-string.js
+++ next-packages/babel-utils/src/object-to-string.js
@@ -1,6 +1,9 @@
 // @flow
 import { serializeStyles } from '@emotion/serialize'
 
+// to anyone looking at this, this isn't intended to simplify every single case
+// it's meant to simplify the most common cases so i don't want to make it especially complex
+// also, this will be unnecessary when prepack is ready
 export function simplifyObject(node: *, t: Object) {
   let bailout = false
   let finalString = ''
