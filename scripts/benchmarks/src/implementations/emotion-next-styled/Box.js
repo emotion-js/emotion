e11402cc304ca7ee9aa4c634c66f87750e245921
mitchellhamilton <mitchell@mitchellhamilton.me>
2018-09-01 15:38:11 +1000

    Fix benchmarks

--- scripts/benchmarks/src/implementations/emotion-next-styled/Box.js
+++ scripts/benchmarks/src/implementations/emotion-next-styled/Box.js
@@ -1,4 +1,4 @@
-import styled from '@emotion/styled.macro'
+import styled from '@emotion/styled/macro'
 import View from './View'
 
 const getColor = ({ color }) => {
--- scripts/benchmarks/src/implementations/emotion-next-styled/View.js
+++ scripts/benchmarks/src/implementations/emotion-next-styled/View.js
@@ -1,4 +1,4 @@
-import styled from '@emotion/styled.macro'
+import styled from '@emotion/styled/macro'
 
 const View = styled.div`
   align-items: stretch;
