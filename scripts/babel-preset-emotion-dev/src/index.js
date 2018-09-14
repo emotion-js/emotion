16a91adc9330a10d74af1300f44244d1dcb2e4bd
Linus Unnebäck <linus@folkdatorn.se>
2018-09-14 14:41:24 +0200

    Replace stage-2 preset with list of plugins

--- scripts/babel-preset-emotion-dev/src/index.js
+++ scripts/babel-preset-emotion-dev/src/index.js
@@ -3,9 +3,23 @@ module.exports = () => {
     presets: [
       ['@babel/preset-env', { loose: true }],
       '@babel/preset-flow',
-      '@babel/preset-react',
-      ['@babel/preset-stage-2', { decoratorsLegacy: true }]
+      '@babel/preset-react'
     ],
-    plugins: ['babel-plugin-codegen']
+    plugins: [
+      'babel-plugin-codegen',
+
+      // Stage 2
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      '@babel/plugin-proposal-function-sent',
+      '@babel/plugin-proposal-export-namespace-from',
+      '@babel/plugin-proposal-numeric-separator',
+      '@babel/plugin-proposal-throw-expressions',
+
+      // Stage 3
+      '@babel/plugin-syntax-dynamic-import',
+      '@babel/plugin-syntax-import-meta',
+      ['@babel/plugin-proposal-class-properties', { loose: false }],
+      '@babel/plugin-proposal-json-strings'
+    ]
   }
 }
