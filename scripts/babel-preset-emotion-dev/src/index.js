a27fb71202efbcb03d2d3d19bccdca588a614604
Linus Unnebäck <linus@folkdatorn.se>
2018-09-14 15:34:06 +0200

    Remove unused Babel plugins

--- scripts/babel-preset-emotion-dev/src/index.js
+++ scripts/babel-preset-emotion-dev/src/index.js
@@ -7,19 +7,7 @@ module.exports = () => {
     ],
     plugins: [
       'babel-plugin-codegen',
-
-      // Stage 2
-      ['@babel/plugin-proposal-decorators', { legacy: true }],
-      '@babel/plugin-proposal-function-sent',
-      '@babel/plugin-proposal-export-namespace-from',
-      '@babel/plugin-proposal-numeric-separator',
-      '@babel/plugin-proposal-throw-expressions',
-
-      // Stage 3
-      '@babel/plugin-syntax-dynamic-import',
-      '@babel/plugin-syntax-import-meta',
-      ['@babel/plugin-proposal-class-properties', { loose: false }],
-      '@babel/plugin-proposal-json-strings'
+      ['@babel/plugin-proposal-class-properties', { loose: false }]
     ]
   }
 }
