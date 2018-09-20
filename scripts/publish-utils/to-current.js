d8fe3baf6d8a2e4d8ae5503e1604cdfca0ce2bcd
mitchellhamilton <mitchell@mitchellhamilton.me>
2018-09-20 17:21:19 +1000

    Change lerna.json, remove stuff that will be unused now and remove bootstrap npm script since yarn does everything

--- scripts/publish-utils/to-current.js
+++
@@ -1,8 +0,0 @@
-// @flow
-const { changeLernaConfig } = require('./change-lerna-config')
-
-changeLernaConfig(lerna => {
-  lerna.packages = ['packages/*']
-  lerna.npmClient = 'npm'
-  delete lerna.useWorkspaces
-})
