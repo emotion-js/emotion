2ec50d29d226efac5a5e016f846c6227d41c1b0d
mitchellhamilton <mitchell@mitchellhamilton.me>
2018-09-23 14:45:47 +1000

    Add some more tests for SSR

--- packages/core/__tests__/server.js
+++ packages/core/__tests__/server.js
@@ -4,134 +4,163 @@
 // @flow
 import 'test-utils/dev-mode'
 import * as React from 'react'
-import cases from 'jest-in-case'
+import testCases from 'jest-in-case'
 import { jsx, Global, keyframes, Provider } from '@emotion/core'
 import styled from '@emotion/styled'
 import css from '@emotion/css'
 import createCache from '@emotion/cache'
 import { renderToString } from 'react-dom/server'
 import HTMLSerializer from 'jest-serializer-html'
+import createEmotionServer from 'create-emotion-server'
 
 expect.addSnapshotSerializer(HTMLSerializer)
 
-cases(
-  'ssr',
-  opts => {
-    expect(renderToString(opts.render())).toMatchSnapshot()
+let cases = {
+  basic: {
+    render: () => <div css={{ color: 'hotpink' }}>some hotpink text</div>
   },
-  {
-    basic: {
-      render: () => <div css={{ color: 'hotpink' }}>some hotpink text</div>
-    },
-    global: {
-      render: () => (
+  global: {
+    render: () => (
+      <Global
+        styles={{
+          html: {
+            backgroundColor: 'hotpink'
+          }
+        }}
+      />
+    )
+  },
+  keyframes: {
+    render: () => {
+      const animation = keyframes(
+        css`
+          from {
+            color: green;
+          }
+          to {
+            color: blue;
+          }
+        `
+      )
+      return (
+        <React.Fragment>
+          <div css={{ animation: `1s ${animation}` }} />
+        </React.Fragment>
+      )
+    }
+  },
+  'only render a style once with the css prop': {
+    render: () => {
+      return (
+        <div css={{ color: 'green' }}>
+          <div css={{ color: 'hotpink' }} />
+          <div css={{ color: 'hotpink' }} />
+        </div>
+      )
+    }
+  },
+  'only render a style once with styled': {
+    render: () => {
+      const SomeComponent = styled.div`
+        color: hotpink;
+      `
+      return (
+        <div css={{ color: 'green' }}>
+          <SomeComponent />
+          <SomeComponent />
+        </div>
+      )
+    }
+  },
+  'works with nonces': {
+    cache: () => createCache({ nonce: 'some-nonce' }),
+    render: () => {
+      const SomeComponent = styled.div`
+        color: hotpink;
+      `
+      return (
+        <React.Fragment>
+          <SomeComponent />
+          <div css={{ color: 'hotpink' }} />
+          <Global
+            styles={{
+              html: {
+                margin: 0,
+                padding: 0,
+                fontFamily: 'sans-serif'
+              }
+            }}
+          />
+        </React.Fragment>
+      )
+    }
+  },
+  'global with keyframes': {
+    render: () => {
+      return (
         <Global
           styles={{
-            html: {
-              backgroundColor: 'hotpink'
+            h1: {
+              animation: `${keyframes({
+                'from,to': {
+                  color: 'green'
+                },
+                '50%': {
+                  color: 'hotpink'
+                }
+              })} 1s`
             }
           }}
         />
       )
-    },
-    keyframes: {
-      render: () => {
-        const animation = keyframes(
-          css`
-            from {
-              color: green;
-            }
-            to {
-              color: blue;
-            }
-          `
-        )
-        return (
-          <React.Fragment>
-            <div css={{ animation: `1s ${animation}` }} />
-          </React.Fragment>
-        )
-      }
-    },
-    'only render a style once with the css prop': {
-      render: () => {
-        return (
-          <div css={{ color: 'green' }}>
-            <div css={{ color: 'hotpink' }} />
-            <div css={{ color: 'hotpink' }} />
-          </div>
-        )
-      }
-    },
-    'only render a style once with styled': {
-      render: () => {
-        const SomeComponent = styled.div`
-          color: hotpink;
-        `
-        return (
-          <div css={{ color: 'green' }}>
-            <SomeComponent />
-            <SomeComponent />
-          </div>
-        )
-      }
-    },
-    'works with nonces': {
-      render: () => {
-        const SomeComponent = styled.div`
-          color: hotpink;
-        `
-        return (
-          <Provider value={createCache({ nonce: 'some-nonce' })}>
-            <SomeComponent />
-            <div css={{ color: 'hotpink' }} />
-            <Global
-              styles={{
-                html: {
-                  margin: 0,
-                  padding: 0,
-                  fontFamily: 'sans-serif'
-                }
-              }}
-            />
-          </Provider>
-        )
-      }
-    },
-    'global with keyframes': {
-      render: () => {
-        return (
-          <Global
-            styles={{
-              h1: {
-                animation: `${keyframes({
-                  'from,to': {
-                    color: 'green'
-                  },
-                  '50%': {
-                    color: 'hotpink'
-                  }
-                })} 1s`
-              }
-            }}
-          />
-        )
-      }
-    },
-    'styled with keyframes': {
-      render: () => {
-        const SomeComponent = styled.div({
-          animation: `${keyframes({
-            'from,to': {
-              color: 'green'
-            },
-            '50%': {
-              color: 'hotpink'
-            }
-          })} 1s`
-        })
-        return <SomeComponent />
-      }
+    }
+  },
+  'styled with keyframes': {
+    render: () => {
+      const SomeComponent = styled.div({
+        animation: `${keyframes({
+          'from,to': {
+            color: 'green'
+          },
+          '50%': {
+            color: 'hotpink'
+          }
+        })} 1s`
+      })
+      return <SomeComponent />
     }
   }
+}
+
+testCases(
+  'ssr',
+  opts => {
+    if (opts.cache) {
+      expect(
+        renderToString(
+          <Provider value={opts.cache()}>{opts.render()}</Provider>
+        )
+      ).toMatchSnapshot()
+    } else {
+      expect(renderToString(opts.render())).toMatchSnapshot()
+    }
+  },
+  cases
+)
+
+testCases(
+  'ssr with old api',
+  opts => {
+    let cache = createCache()
+    if (opts.cache) {
+      cache = opts.cache()
+    }
+    let { renderStylesToString } = createEmotionServer(cache)
+    expect(
+      renderStylesToString(
+        renderToString(<Provider value={cache}>{opts.render()}</Provider>)
+      )
+    ).toMatchSnapshot()
+  },
+  cases
 )
