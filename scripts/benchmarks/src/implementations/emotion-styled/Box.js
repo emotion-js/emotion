d43da62402b8581571736263220ca26f81f03e69
Mitchell Hamilton <mitchell@mitchellhamilton.me>
2018-10-16 15:26:12 +1000

    Remove irrelevant benchmarks (#931)
    * Remove irrelevant benchmarks
    
    * Fix benchmarks and rename emotion benchmark to emotion-css-func

--- scripts/benchmarks/src/implementations/emotion-styled/Box.js
+++ scripts/benchmarks/src/implementations/emotion-styled/Box.js
@@ -1,4 +1,4 @@
-import styled from 'react-emotion'
+import styled from '@emotion/styled/macro'
 import View from './View'
 
 const getColor = ({ color }) => {
--- scripts/benchmarks/src/implementations/emotion-styled/View.js
+++ scripts/benchmarks/src/implementations/emotion-styled/View.js
@@ -1,6 +1,6 @@
-import styled from 'react-emotion'
+import styled from '@emotion/styled/macro'
 
-const View = styled('div')`
+const View = styled.div`
   align-items: stretch;
   border-width: 0;
   border-style: solid;
