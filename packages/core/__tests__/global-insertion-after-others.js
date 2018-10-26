44f6c7e6eb830bebed6d688053dd7e6d2b898949
mitchellhamilton <mitchell@hamil.town>
2018-10-26 21:21:09 +1000

    Add a test

---
+++ packages/core/__tests__/global-insertion-after-others.js
@@ -0,0 +1,33 @@
+/** @jsx jsx */
+import { jsx, Global } from '@emotion/core'
+import { render } from 'react-testing-library'
+
+let getDataAttributes = () =>
+  Array.from(document.querySelectorAll('style[data-emotion]'), x =>
+    x.getAttribute('data-emotion')
+  )
+
+test('Global style element insertion after insertion of other styles', () => {
+  let Comp = ({ second }) => (
+    <div>
+      <div
+        css={{
+          color: 'green'
+        }}
+      />
+      {second && (
+        <Global
+          styles={{
+            html: {
+              backgroundColor: 'hotpink'
+            }
+          }}
+        />
+      )}
+    </div>
+  )
+  let { rerender } = render(<Comp />)
+  expect(getDataAttributes()).toEqual(['css'])
+  rerender(<Comp second />)
+  expect(getDataAttributes()).toEqual(['css-global', 'css'])
+})
