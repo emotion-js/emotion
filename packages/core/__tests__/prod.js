emotion-js/emotion

commit 45cc67be66883fc96ef6b607844c267c82d2eba2

Auto label for css prop without babel plugin (#976)
* Auto label for css prop without babel plugin

* Change regex

* Add a test

* Remove try catch

packages/core/src/jsx.js
@@ -6,6 +6,8 @@ import { serializeStyles } from '@emotion/serialize'
 
 let typePropName = '__EMOTION_TYPE_PLEASE_DO_NOT_USE__'
 
+let labelPropName = '__EMOTION_LABEL_PLEASE_DO_NOT_USE__'
+
 let hasOwnProperty = Object.prototype.hasOwnProperty
 
 let render = (cache, props, theme: null | Object, ref) => {
@@ -21,7 +23,20 @@ let render = (cache, props, theme: null | Object, ref) => {
     )
   }
   registeredStyles.push(theme === null ? props.css : props.css(theme))
-  const serialized = serializeStyles(cache.registered, registeredStyles)
+  let serialized = serializeStyles(cache.registered, registeredStyles)
+
+  if (
+    process.env.NODE_ENV !== 'production' &&
+    serialized.name.indexOf('-') === -1
+  ) {
+    let labelFromStack = props[labelPropName]
+    if (labelFromStack) {
+      serialized = serializeStyles(cache.registered, [
+        serialized,
+        'label:' + labelFromStack + ';'
+      ])
+    }
+  }
   const rules = insertStyles(cache, serialized, typeof type === 'string')
   className += `${cache.key}-${serialized.name}`
 
@@ -30,7 +45,8 @@ let render = (cache, props, theme: null | Object, ref) => {
     if (
       hasOwnProperty.call(props, key) &&
       key !== 'css' &&
-      key !== typePropName
+      key !== typePropName &&
+      (process.env.NODE_ENV !== 'production' && key !== labelPropName)
     ) {
       newProps[key] = props[key]
     }
@@ -87,8 +103,8 @@ export const jsx: typeof React.createElement = function(
   }
 
   if (
-    typeof props.css === 'string' &&
     process.env.NODE_ENV !== 'production' &&
+    typeof props.css === 'string' &&
     // check if there is a css declaration
     props.css.indexOf(':') !== -1
   ) {
@@ -112,6 +128,20 @@ export const jsx: typeof React.createElement = function(
     }
   }
   newProps[typePropName] = type
+  if (process.env.NODE_ENV !== 'production') {
+    let error = new Error()
+    if (error.stack) {
+      // chrome
+      let match = error.stack.match(/at jsx.*\n\s+at ([A-Z][A-Za-z]+) /)
+      if (!match) {
+        // safari and firefox
+        match = error.stack.match(/^.*\n([A-Z][A-Za-z]+)@/)
+      }
+      if (match) {
+        newProps[labelPropName] = match[1]
+      }
+    }
+  }
 
   createElementArgArray[1] = newProps
 

packages/core/__tests__/css.js
@@ -137,3 +137,21 @@ test('can set speedy via custom cache', () => {
   )
   expect(cache.sheet.tags).toHaveLength(1)
 })
+
+test('autoLabel without babel', () => {
+  let SomeComp = props => {
+    return (
+      <div
+        {...props}
+        css={{
+          color: 'hotpink'
+        }}
+      >
+        something
+      </div>
+    )
+  }
+  const tree = renderer.create(<SomeComp />)
+
+  expect(tree.toJSON().props.className.endsWith('-SomeComp')).toBe(true)
+})

commit ec096a2d334378eb41250813353a1ee3715450f6

Fix site and css prop regression introduced by #976 (#979)
* Fix site

* Fix props in production with css prop

packages/core/src/jsx.js
@@ -46,7 +46,7 @@ let render = (cache, props, theme: null | Object, ref) => {
       hasOwnProperty.call(props, key) &&
       key !== 'css' &&
       key !== typePropName &&
-      (process.env.NODE_ENV !== 'production' && key !== labelPropName)
+      (process.env.NODE_ENV === 'production' || key !== labelPropName)
     ) {
       newProps[key] = props[key]
     }

site/plugins/gatsby-remark-live-code/index.js
@@ -24,7 +24,9 @@ module.exports = ({ markdownAST }) => {
       node.value = escapeGoat.escapeTag`<live-code code="${cleanValue}" compiled="${
         Babel.transform(cleanValue, {
           presets: ['es2015', 'react', 'stage-1'],
-          plugins: [require('babel-plugin-emotion').default]
+          plugins: [
+            [require('babel-plugin-emotion').default, { sourceMap: false }]
+          ]
         }).code
       }"></live-code>`
     }

packages/core/__tests__/prod.js
@@ -1,5 +1,7 @@
 import 'test-utils/prod-mode'
-import { css } from '@emotion/core'
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+import renderer from 'react-test-renderer'
 
 test('css works', () => {
   // css has a different return in prod so this is just making sure that isn't broken
@@ -11,4 +13,9 @@ Object {
   "styles": "color:hotpink;",
 }
 `)
+})
+
+test('props work', () => {
+  let tree = renderer.create(<div css={{ color: 'hotpink' }} hidden />)
+  expect(tree.toJSON().props.hidden).toBe(true)
 })