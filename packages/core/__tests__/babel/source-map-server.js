e592c32db65fde5183420be47a7ff90525e629a2
mitchellhamilton <mitchell@hamil.town>
2018-11-04 08:24:50 +1000

    Fix test

--- packages/core/__tests__/babel/source-map-server.js
+++ packages/core/__tests__/babel/source-map-server.js
@@ -1,14 +1,23 @@
-// @flow
 /** @jsx jsx
  * @jest-environment node
  */
+// @flow
 import 'test-utils/dev-mode'
 import { jsx } from '@emotion/core'
 import { renderToString } from 'react-dom/server'
 
 test('basic', () => {
   let html = renderToString(
-    <div css={{ color: 'hotpink' }}>some hotpink text</div>
+    <div
+      css={{
+        color: 'hotpink',
+        ':hover': {
+          color: 'green'
+        }
+      }}
+    >
+      some hotpink text
+    </div>
   )
   expect(html).toMatchSnapshot()
 })
