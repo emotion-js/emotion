3133ce1c9dbfbac468b60d89e43ab8af8386b961
mitchellhamilton <mitchell@hamil.town>
2018-11-13 09:26:09 +1000

    Upgrade flow again

--- packages/weak-memoize/src/index.js
+++ packages/weak-memoize/src/index.js
@@ -1,5 +1,6 @@
 // @flow
 let weakMemoize = function<Arg, Return>(func: Arg => Return): Arg => Return {
+  // $FlowFixMe flow doesn't include all non-primitive types as allowed for weakmaps
   let cache: WeakMap<Arg, Return> = new WeakMap()
   return arg => {
     if (cache.has(arg)) {
