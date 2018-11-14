fb80c95939c0f887f53c9df796b35f15fd7fafde
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-11-14 05:47:40 +0100

    Add support for `as` prop (#843)
    * Add support for `as` prop
    
    * Tweak as prop & shouldForwardProp interoperability
    
    * Removed unused `optionsShouldForwardProp` variable
    
    * Move and rewrite tests so it's easy to see what the expected behaviour is, fix flow errors and hoist a thing
    
    * Update behaviour
    
    * Fix shouldForwardProp

--- packages/styled-base/src/utils.js
+++ packages/styled-base/src/utils.js
@@ -4,9 +4,18 @@ import isPropValid from '@emotion/is-prop-valid'
 
 export type Interpolations = Array<any>
 
-export const testOmitPropsOnStringTag: (key: string) => boolean = isPropValid
-export const testOmitPropsOnComponent = (key: string) =>
+const testOmitPropsOnStringTag = isPropValid
+const testOmitPropsOnComponent = (key: string) =>
   key !== 'theme' && key !== 'innerRef'
+
+export const getDefaultShouldForwardProp = (tag: React.ElementType) =>
+  typeof tag === 'string' &&
+  // 96 is one less than the char code
+  // for "a" so this is checking that
+  // it's a lowercase character
+  tag.charCodeAt(0) > 96
+    ? testOmitPropsOnStringTag
+    : testOmitPropsOnComponent
 
 export type StyledOptions = {
   label?: string,
