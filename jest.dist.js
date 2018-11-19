26d7368556392dbf5f8a89cc6c2f9c467b6eb0a5
Mitchell Hamilton <mitchell@hamil.town>
2018-11-19 20:44:26 +1000

    Use preconstruct (#1023)
    * Fix packages
    
    * Update preconstruct
    
    * Remove old build script
    
    * Change npm scripts
    
    * Use aliases from preconstruct to allow other entrypoints in the future
    
    * Update preconstruct

--- jest.dist.js
+++ jest.dist.js
@@ -1,8 +1,6 @@
-const { jest: lernaAliases } = require('lerna-alias')
 const baseConfig = require('./jest.config.js')
 
 module.exports = Object.assign({}, baseConfig, {
-  moduleNameMapper: lernaAliases({ sourceDirectory: false }),
   transformIgnorePatterns: ['dist', 'node_modules'],
   testPathIgnorePatterns: baseConfig.testPathIgnorePatterns.concat(
     'babel-plugin-emotion'
