0a68bc91a014613f7fe3049ef74f38d59f68c0fe
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-11-28 23:58:42 +0100

    Handle handlers pattern with charcodes test

--- packages/is-prop-valid/src/index.js
+++ packages/is-prop-valid/src/index.js
@@ -4,4 +4,11 @@ declare var codegen: { require: string => RegExp }
 
 const reactPropsRegex = codegen.require('./props')
 
-export default memoize(reactPropsRegex.test.bind(reactPropsRegex))
+// https://esbench.com/bench/5bfee68a4cd7e6009ef61d23
+export default memoize(
+  prop =>
+    reactPropsRegex.test(prop) ||
+    (prop.charCodeAt(0) === 111 /* o */ &&
+    prop.charCodeAt(1) === 110 /* n */ &&
+      prop.charCodeAt(2) < 91) /* Z+1 */
+)
