6e43050bd90ad79be6652fa5c3cc6d746fed4882
mitchellhamilton <mitchell@hamil.town>
2019-02-10 21:30:47 +1000

    Refactor styled macro

--- packages/babel-plugin-emotion/src/styled-macro.js
+++ packages/babel-plugin-emotion/src/styled-macro.js
@@ -3,6 +3,101 @@ import { createMacro } from 'babel-plugin-macros'
 import { addDefault, addNamed } from '@babel/helper-module-imports'
 import { transformExpressionWithStyles, getStyledOptions } from './utils'
 
+function addImport(
+  state: any,
+  importPath: string,
+  imported: string,
+  nameHint?: string
+) {
+  let cacheKey = ['import', importPath, imported].join(':')
+  if (state[cacheKey] === undefined) {
+    let importIdentifier
+    if (imported === 'default') {
+      importIdentifier = addDefault(state.file.path, importPath, { nameHint })
+    } else {
+      importIdentifier = addNamed(state.file.path, imported, importPath, {
+        nameHint
+      })
+    }
+    state[cacheKey] = importIdentifier.name
+  }
+  return {
+    type: 'Identifier',
+    name: state[cacheKey]
+  }
+}
+
+export let styledTransformer = ({
+  state,
+  babel,
+  importPath,
+  reference,
+  options: { baseImportPath, isWeb }
+}: Object) => {
+  let getStyledIdentifier = () => {
+    return addImport(state, baseImportPath, 'default', 'styled')
+  }
+  let getOriginalImportPathStyledIdentifier = () => {
+    return addImport(state, importPath, 'default', 'styled')
+  }
+  let t = babel.types
+  let isCall = false
+  if (
+    t.isMemberExpression(reference.parent) &&
+    reference.parent.computed === false
+  ) {
+    isCall = true
+    if (
+      // checks if the first character is lowercase
+      // becasue we don't want to transform the member expression if
+      // it's in primitives/native
+      reference.parent.property.name.charCodeAt(0) > 96
+    ) {
+      reference.parentPath.replaceWith(
+        t.callExpression(getStyledIdentifier(), [
+          t.stringLiteral(reference.parent.property.name)
+        ])
+      )
+    } else {
+      reference.replaceWith(getStyledIdentifier())
+    }
+  } else if (
+    reference.parentPath &&
+    reference.parentPath.parentPath &&
+    t.isCallExpression(reference.parentPath) &&
+    reference.parent.callee === reference.node
+  ) {
+    isCall = true
+    reference.replaceWith(getStyledIdentifier())
+  } else {
+    reference.replaceWith(getOriginalImportPathStyledIdentifier())
+  }
+  if (reference.parentPath && reference.parentPath.parentPath) {
+    const styledCallPath = reference.parentPath.parentPath
+    let { node } = transformExpressionWithStyles({
+      path: styledCallPath,
+      state,
+      babel,
+      shouldLabel: false
+    })
+    if (node && isWeb) {
+      // we know the argument length will be 1 since that's the only time we will have a node since it will be static
+      styledCallPath.node.arguments[0] = node
+    }
+  }
+
+  if (isCall) {
+    reference.addComment('leading', '#__PURE__')
+    if (isWeb) {
+      reference.parentPath.node.arguments[1] = getStyledOptions(
+        t,
+        reference.parentPath,
+        state
+      )
+    }
+  }
+}
+
 export let createStyledMacro = ({
   importPath,
   originalImportPath = importPath,
@@ -18,87 +113,14 @@ export let createStyledMacro = ({
     }
     const t = babel.types
     if (references.default && references.default.length) {
-      let _styledIdentifier
-      let getStyledIdentifier = () => {
-        if (_styledIdentifier === undefined) {
-          _styledIdentifier = addDefault(state.file.path, importPath, {
-            nameHint: 'styled'
-          })
-        }
-        return t.cloneDeep(_styledIdentifier)
-      }
-      let originalImportPathStyledIdentifier
-      let getOriginalImportPathStyledIdentifier = () => {
-        if (originalImportPathStyledIdentifier === undefined) {
-          originalImportPathStyledIdentifier = addDefault(
-            state.file.path,
-            originalImportPath,
-            {
-              nameHint: 'styled'
-            }
-          )
-        }
-        return t.cloneDeep(originalImportPathStyledIdentifier)
-      }
-      if (importPath === originalImportPath) {
-        getOriginalImportPathStyledIdentifier = getStyledIdentifier
-      }
       references.default.forEach(reference => {
-        let isCall = false
-        if (
-          t.isMemberExpression(reference.parent) &&
-          reference.parent.computed === false
-        ) {
-          isCall = true
-          if (
-            // checks if the first character is lowercase
-            // becasue we don't want to transform the member expression if
-            // it's in primitives/native
-            reference.parent.property.name.charCodeAt(0) > 96
-          ) {
-            reference.parentPath.replaceWith(
-              t.callExpression(getStyledIdentifier(), [
-                t.stringLiteral(reference.parent.property.name)
-              ])
-            )
-          } else {
-            reference.replaceWith(getStyledIdentifier())
-          }
-        } else if (
-          reference.parentPath &&
-          reference.parentPath.parentPath &&
-          t.isCallExpression(reference.parentPath) &&
-          reference.parent.callee === reference.node
-        ) {
-          isCall = true
-          reference.replaceWith(getStyledIdentifier())
-        } else {
-          reference.replaceWith(getOriginalImportPathStyledIdentifier())
-        }
-        if (reference.parentPath && reference.parentPath.parentPath) {
-          const styledCallPath = reference.parentPath.parentPath
-          let { node } = transformExpressionWithStyles({
-            path: styledCallPath,
-            state,
-            babel,
-            shouldLabel: false
-          })
-          if (node && isWeb) {
-            // we know the argument length will be 1 since that's the only time we will have a node since it will be static
-            styledCallPath.node.arguments[0] = node
-          }
-        }
-
-        if (isCall) {
-          reference.addComment('leading', '#__PURE__')
-          if (isWeb) {
-            reference.parentPath.node.arguments[1] = getStyledOptions(
-              t,
-              reference.parentPath,
-              state
-            )
-          }
-        }
+        styledTransformer({
+          state,
+          babel,
+          reference,
+          importPath: originalImportPath,
+          options: { baseImportPath: importPath, isWeb }
+        })
       })
     }
     Object.keys(references)
