3e235ac58f8f08adb5b1e6cc5ae764c5501bc547
mitchellhamilton <mitchell@hamil.town>
2019-03-05 19:27:33 +1000

    Skip printing css prop internal component

--- packages/jest-emotion/test/react-enzyme.test.js
+++ packages/jest-emotion/test/react-enzyme.test.js
@@ -1,7 +1,7 @@
 import 'test-utils/legacy-env'
 /** @jsx jsx */
 import * as enzyme from 'enzyme'
-import { css, jsx } from '@emotion/core'
+import { jsx } from '@emotion/core'
 import { createSerializer as createEnzymeSerializer } from 'enzyme-to-json'
 import { createSerializer } from 'jest-emotion'
 
@@ -10,7 +10,7 @@ expect.addSnapshotSerializer(createSerializer())
 
 test('enzyme mount test', () => {
   const Greeting = ({ children }) => (
-    <div css={css({ backgroundColor: 'red' })}>{children}</div>
+    <div css={{ backgroundColor: 'red' }}>{children}</div>
   )
   const tree = enzyme.mount(<Greeting>hello</Greeting>)
   expect(tree).toMatchSnapshot()
