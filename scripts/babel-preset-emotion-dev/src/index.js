0b2b168172be4ca920ca71929ad26f18ef2b5444
mitchellhamilton <mitchell@hamil.town>
2019-04-11 16:11:06 +1000

    Plugin systems.....

--- scripts/babel-preset-emotion-dev/src/index.js
+++ scripts/babel-preset-emotion-dev/src/index.js
@@ -5,17 +5,17 @@ module.exports = () => {
       require.resolve('@babel/preset-react')
     ],
     plugins: [
+      require.resolve(
+        'babel-plugin-add-basic-constructor-for-react-components'
+      ),
+      require.resolve('babel-plugin-fix-dce-for-classes-with-statics'),
       require.resolve('@babel/plugin-transform-flow-strip-types'),
       require.resolve('babel-plugin-codegen'),
       require.resolve('@babel/plugin-transform-runtime'),
       [
         require.resolve('@babel/plugin-proposal-class-properties'),
         { loose: true }
-      ],
-      require.resolve(
-        'babel-plugin-add-basic-constructor-for-react-components'
-      ),
-      require.resolve('babel-plugin-fix-dce-for-classes-with-statics')
+      ]
     ]
   }
 }
