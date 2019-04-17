51e48563e22be9a2cae9bcf2cc9ac2f1deca78a0
Mitchell Hamilton <mitchell@hamil.town>
2019-04-17 14:29:56 +1000

    Use preconstruct dev command (#1317)
    * test
    
    * Try a new thing
    
    * Try removing more stuff
    
    * Upgrade preconstruct and remove flow mapping stuff
    
    * Add max workers thing back to .flowconfig-ci

--- scripts/babel-plugin-emotion-test/src/index.js
+++
@@ -1,6 +0,0 @@
-require('@babel/register')
-let aliases = require('preconstruct').aliases.rollup()
-delete aliases['emotion']
-require('module-alias').addAliases(aliases)
-
-module.exports = require('babel-plugin-emotion')
