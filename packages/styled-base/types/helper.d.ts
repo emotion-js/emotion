1e605f421db206a6e0e256e1ad6999c566007879
Ryan Swanson <ryan.swanson@theice.com>
2019-06-18 12:09:09 -0400

    PropsOf<C> TypeScript utilities - Support defaultProps by delegating to new JSX and React types.

--- packages/styled-base/types/helper.d.ts
+++ packages/styled-base/types/helper.d.ts
@@ -4,14 +4,8 @@ import * as React from 'react'
  * @desc Utility type for getting props type of React component.
  */
 export type PropsOf<
-  Tag extends React.ComponentType<any>
-> = Tag extends React.SFC<infer Props>
-  ? Props & React.Attributes
-  : Tag extends React.ComponentClass<infer Props>
-    ? (Tag extends new (...args: Array<any>) => infer Instance
-        ? Props & React.ClassAttributes<Instance>
-        : never)
-    : never
+  C extends keyof JSX.IntrinsicElements | React.JSXElementConstructor<any>
+> = JSX.LibraryManagedAttributes<C, React.ComponentPropsWithRef<C>>
 
 export type Omit<T, U> = T extends any ? Pick<T, Exclude<keyof T, U>> : never
 export type Overwrapped<T, U> = Pick<T, Extract<keyof T, keyof U>>
