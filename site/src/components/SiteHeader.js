40591350c0f5ab5742c736dbf3d6ac39462314d7
Mihira Jayasekera <mihira@tinymachine.net>
2019-06-24 14:47:09 -0700

    Prevent site header from breaking mobile layout
    The width of the nav links in the site header breaks the layout in viewports narrower than ~520pts. Allow the navigation links to horizontally scroll to prevent the entire site layout from scrolling horizontally.

--- site/src/components/SiteHeader.js
+++ site/src/components/SiteHeader.js
@@ -60,7 +60,9 @@ export default function SiteHeader() {
       </Link>
       <nav
         css={{
-          marginLeft: 'auto'
+          marginLeft: 'auto',
+          overflowX: 'auto',
+          WebkitOverflowScrolling: 'touch'
         }}
       >
         <ul
@@ -75,7 +77,13 @@ export default function SiteHeader() {
             padding: 0,
             margin: '0 0 0 auto',
             overflow: 'auto',
-            listStyle: 'none'
+            listStyle: 'none',
+            'li a': {
+              display: 'inline-block',
+              paddingTop: space[1],
+              paddingBottom: space[1],
+              whiteSpace: 'nowrap'
+            }
           }}
         >
           <li>
