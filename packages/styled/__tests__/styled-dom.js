48162c5b0b2c5ce151a9a9fdd62b5394b862a269
Mateusz Burzyński <mateuszburzynski@gmail.com>
2019-06-25 09:57:42 +0200

    Migrate to scoped @testing-library/react

--- packages/styled/__tests__/styled-dom.js
+++ packages/styled/__tests__/styled-dom.js
@@ -1,7 +1,7 @@
 // @flow
 import * as React from 'react'
 import styled from '@emotion/styled'
-import { render, cleanup } from 'react-testing-library'
+import { render, cleanup } from '@testing-library/react'
 import { ignoreConsoleErrors } from 'test-utils'
 
 afterEach(cleanup)
