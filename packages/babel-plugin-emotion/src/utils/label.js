2c86d5d3eda19d18e3fad1de3bda939c113688df
Andrew Hutchings <andrew.hutchings@gmail.com>
2019-10-01 17:15:55 -0400

    babel-plugin-emotion: Fix `autoLabel` crash on `css` used in expression assigned to an object pattern
    Ensure that the variable declarator's id is an identifier before using its name as the label.

--- packages/babel-plugin-emotion/src/utils/label.js
+++ packages/babel-plugin-emotion/src/utils/label.js
@@ -59,7 +59,10 @@ function getDeclaratorName(path, t) {
   // we probably have a css call assigned to a variable
   // so we'll just return the variable name
   if (parent.isVariableDeclarator()) {
-    return parent.node.id.name
+    if (t.isIdentifier(parent.node.id)) {
+      return parent.node.id.name
+    }
+    return ''
   }
 
   // we probably have an inline css prop usage
