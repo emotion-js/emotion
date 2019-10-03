6a54a7b4e5d241dd70329f9943b0e2cceb4697d1
Jake Ginnivan <jake.ginnivan@wanews.com.au>
2019-10-03 13:19:06 +0800

    Reverted TypeScript version bump in create-emotion types

--- packages/create-emotion/types/index.d.ts
+++ packages/create-emotion/types/index.d.ts
@@ -1,5 +1,5 @@
 // Definitions by: Junyoung Clare Jang <https://github.com/Ailrun>
-// TypeScript Version: 2.9
+// TypeScript Version: 2.8
 
 import { EmotionCache, Options } from '@emotion/cache'
 import { Interpolation } from '@emotion/serialize'
