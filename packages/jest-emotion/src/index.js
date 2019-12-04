490408d9332a90ef75915ea5f8533e622e97ef73
Andrew Stephens <astephens@cainc.com>
2019-12-04 12:52:12 -0500

    Improve support for Enzyme's shallow rendering

--- packages/jest-emotion/src/index.js
+++ packages/jest-emotion/src/index.js
@@ -9,10 +9,14 @@ import {
   isDOMElement,
   getStylesFromClassNames,
   getStyleElements,
-  getKeys
+  getKeys,
+  flatMap,
+  isPrimitive,
+  hasIntersection
 } from './utils'
-
 export { matchers } from './matchers'
+
+const TRANSFORMED = Symbol.for('__EMOTION_ENZMYE_TRANSFORMED__')
 
 function getNodes(node, nodes = []) {
   if (Array.isArray(node)) {
@@ -35,11 +39,42 @@ function getNodes(node, nodes = []) {
   return nodes
 }
 
+function deepTransform(node, transform) {
+  if (Array.isArray(node)) {
+    return node.map(child => deepTransform(child, transform))
+  }
+
+  const transformed: any = transform(node)
+
+  if (transformed.props) {
+    transformed.props = Object.entries(transformed.props).reduce(
+      (props, [key, value]) =>
+        Object.assign(props, {
+          [key]: deepTransform(value, transform)
+        }),
+      {}
+    )
+  }
+
+  if (transformed.children) {
+    return {
+      ...transformed,
+      // flatMap to allow a child of <A><B /><C /></A> to be transformed to <B /><C />
+      children: flatMap(
+        deepTransform(transformed.children, transform),
+        id => id
+      )
+    }
+  }
+
+  return transformed
+}
+
 function getPrettyStylesFromClassNames(
   classNames: Array<string>,
   elements: Array<HTMLStyleElement>
 ) {
-  let styles = getStylesFromClassNames(classNames, elements)
+  const styles = getStylesFromClassNames(classNames, elements)
 
   let prettyStyles
   try {
@@ -69,51 +104,122 @@ function filterEmotionProps(props = {}) {
   return rest
 }
 
+function isShallowEnzymeElement(element, classNames) {
+  const delimiter = ' '
+  const childClassNames = flatMap(element.children || [], ({ props = {} }) =>
+    (props.className || '').split(delimiter)
+  ).filter(Boolean)
+
+  return !hasIntersection(classNames, childClassNames)
+}
+
+const creatConvertEmotionElements = (keys, printer) => node => {
+  if (isPrimitive(node)) {
+    return node
+  }
+  if (isEmotionCssPropEnzymeElement(node)) {
+    const cssClassNames = (node.props.css.name || '').split(' ')
+    const expectedClassNames = flatMap(cssClassNames, cssClassName =>
+      keys.map(key => `${key}-${cssClassName}`)
+    )
+    // if this is a shallow element, we need to manufacture the className
+    // since the underlying component is not rendered.
+    if (isShallowEnzymeElement(node, expectedClassNames)) {
+      const className = [node.props.className]
+        .concat(expectedClassNames)
+        .filter(Boolean)
+        .join(' ')
+      const emotionType = node.props.__EMOTION_TYPE_PLEASE_DO_NOT_USE__
+      // emotionType will be a string for DOM elements
+      const type =
+        typeof emotionType === 'string' ? emotionType : emotionType.name
+      return {
+        ...node,
+        props: filterEmotionProps({
+          ...node.props,
+          className
+        }),
+        type,
+        [TRANSFORMED]: true
+      }
+    } else {
+      return node.children
+    }
+  }
+  if (isEmotionCssPropElementType(node)) {
+    return {
+      ...node,
+      props: filterEmotionProps(node.props),
+      type: node.props.__EMOTION_TYPE_PLEASE_DO_NOT_USE__,
+      [TRANSFORMED]: true
+    }
+  }
+  return {
+    ...node,
+    [TRANSFORMED]: true
+  }
+}
+
+function clean(node: any, classNames: string[]) {
+  if (Array.isArray(node)) {
+    for (const child of node) {
+      clean(child, classNames)
+    }
+    return
+  }
+  if (node.children) {
+    for (const child of node.children) {
+      clean(child, classNames)
+    }
+  }
+  const hasKnownClass =
+    node.props &&
+    node.props.className &&
+    hasIntersection(node.props.className.split(' '), classNames)
+  if (hasKnownClass) {
+    delete node.props.css
+  }
+}
+
 export function createSerializer({
   classNameReplacer,
   DOMElements = true
 }: Options = {}) {
-  let cache = new WeakSet()
+  const cache = new WeakSet()
   function print(val: *, printer: Function) {
-    if (isEmotionCssPropEnzymeElement(val)) {
-      return val.children.map(printer).join('\n')
-    }
-    if (isEmotionCssPropElementType(val)) {
-      return printer({
-        ...val,
-        props: filterEmotionProps(val.props),
-        type: val.props.__EMOTION_TYPE_PLEASE_DO_NOT_USE__
-      })
-    }
-    const nodes = getNodes(val)
-    const classNames = getClassNamesFromNodes(nodes)
-    let elements = getStyleElements()
-    const styles = getPrettyStylesFromClassNames(classNames, elements)
+    const seen = cache.has(val)
+    const elements = getStyleElements()
+    const keys = getKeys(elements)
+    const convertEmotionElements = creatConvertEmotionElements(keys, printer)
+    const converted = deepTransform(val, convertEmotionElements)
+    const nodes = getNodes(converted)
     nodes.forEach(cache.add, cache)
-    const printedVal = printer(val)
+    const classNames = getClassNamesFromNodes(nodes)
+    const styles = getPrettyStylesFromClassNames(classNames, elements)
+    clean(converted, classNames)
+    const printedVal = printer(converted)
     nodes.forEach(cache.delete, cache)
-    let keys = getKeys(elements)
-    return replaceClassNames(
-      classNames,
-      styles,
-      printedVal,
-      keys,
-      classNameReplacer
-    )
+    return seen
+      ? printedVal
+      : replaceClassNames(
+          classNames,
+          styles,
+          printedVal,
+          keys,
+          classNameReplacer
+        )
   }
 
   function test(val: *) {
     return (
       val &&
-      ((!cache.has(val) &&
-        (isReactElement(val) || (DOMElements && isDOMElement(val)))) ||
-        isEmotionCssPropEnzymeElement(val) ||
-        isEmotionCssPropElementType(val))
+      (!val[TRANSFORMED] &&
+        (isReactElement(val) || (DOMElements && isDOMElement(val))))
     )
   }
   return { test, print }
 }
 
-export let { print, test } = createSerializer()
+export const { print, test } = createSerializer()
 
 export default { print, test }
--- packages/jest-emotion/test/react-enzyme.test.js
+++ packages/jest-emotion/test/react-enzyme.test.js
@@ -1,130 +1,177 @@
-import 'test-utils/legacy-env'
+import 'test-utils/legacy-env' // adds serializer
 /** @jsx jsx */
+import jestInCase from 'jest-in-case'
 import * as enzyme from 'enzyme'
 import { jsx } from '@emotion/core'
-import { createSerializer as createEnzymeSerializer } from 'enzyme-to-json'
-import { createSerializer } from 'jest-emotion'
-import { toMatchSnapshot } from 'jest-snapshot'
+import styled from '@emotion/styled'
 import React from 'react'
+import toJson from 'enzyme-to-json'
 
-const createEnzymeSnapshotMatcher = serializerOptions => {
-  const serializer = createEnzymeSerializer(serializerOptions)
-  const identityPrinter = v => v
+const tickle = (wrapper: *) => {
+  wrapper.find('EmotionCssPropInternal').forEach(el => el.dive())
+  return wrapper
+}
 
-  return function(val) {
-    return toMatchSnapshot.call(this, serializer.print(val, identityPrinter))
+const cases = {
+  basic: {
+    render() {
+      const Greeting = ({ children }) => (
+        <div css={{ backgroundColor: 'red' }}>{children}</div>
+      )
+      return <Greeting>hello</Greeting>
+    }
+  },
+  styled: {
+    render() {
+      const Greeting = styled.div`
+        background-color: red;
+      `
+      return <Greeting>Hello</Greeting>
+    }
+  },
+  nested: {
+    render() {
+      return <div>{cases.basic.render()}</div>
+    }
+  },
+  'nested styled': {
+    render() {
+      return <div>{cases.styled.render()}</div>
+    }
+  },
+  'with styles on top level': {
+    render() {
+      const Greeting = ({ children, className }) => (
+        <div className={className}>{children}</div>
+      )
+      return <Greeting css={{ backgroundColor: 'red' }}>Hello</Greeting>
+    }
+  },
+  'with prop containing css element': {
+    render() {
+      const Greeting = ({ children, content }) => (
+        <div>
+          {content} {children}
+        </div>
+      )
+      return (
+        <Greeting content={<p css={{ backgroundColor: 'blue' }}>Hello</p>}>
+          World!
+        </Greeting>
+      )
+    }
+  },
+  'with prop containing css element not at the top level': {
+    render() {
+      const Greeting = ({ children, content }) => (
+        <div>
+          {content} {children}
+        </div>
+      )
+
+      return (
+        <div>
+          <Greeting
+            content={
+              <p id="something" css={{ backgroundColor: 'blue' }}>
+                Hello
+              </p>
+            }
+          >
+            World!
+          </Greeting>
+        </div>
+      )
+    }
+  },
+  'with prop containing css element with other props': {
+    render() {
+      const Greeting = ({ children, content }) => (
+        <div>
+          {content} {children}
+        </div>
+      )
+
+      return (
+        <Greeting
+          content={
+            <p id="something" css={{ backgroundColor: 'blue' }}>
+              Hello
+            </p>
+          }
+        >
+          World!
+        </Greeting>
+      )
+    }
+  },
+  'with prop containing css element with other label': {
+    render() {
+      const Thing = ({ content, children }) => {
+        return children
+      }
+      const Greeting = ({ children, content }) => (
+        <Thing content={<div css={{ color: 'hotpink' }} />}>
+          {content} {children}
+        </Thing>
+      )
+
+      return (
+        <Greeting
+          content={
+            <p id="something" css={{ backgroundColor: 'blue' }}>
+              Hello
+            </p>
+          }
+        >
+          World!
+        </Greeting>
+      )
+    }
   }
 }
 
-expect.addSnapshotSerializer(createSerializer())
-
-expect.extend({
-  toMatchShallowSnapshot: createEnzymeSnapshotMatcher(),
-  toMatchDeepSnapshot: createEnzymeSnapshotMatcher({ mode: 'deep' })
-})
-
-test('enzyme mount test', () => {
-  const Greeting = ({ children }) => (
-    <div css={{ backgroundColor: 'red' }}>{children}</div>
-  )
-  const tree = enzyme.mount(<Greeting>hello</Greeting>)
-  expect(tree).toMatchShallowSnapshot()
-})
-
-test('enzyme test with prop containing css element', () => {
-  const Greeting = ({ children, content }) => (
-    <div>
-      {content} {children}
-    </div>
+describe('enzyme', () => {
+  jestInCase(
+    'shallow',
+    ({ render }) => {
+      const wrapper = enzyme.shallow(render())
+      expect(toJson(wrapper)).toMatchSnapshot()
+    },
+    cases
   )
 
-  const tree = enzyme.mount(
-    <Greeting content={<p css={{ backgroundColor: 'blue' }}>Hello</p>}>
-      World!
-    </Greeting>
-  )
-  expect(tree).toMatchShallowSnapshot()
-})
-
-test('enzyme test with prop containing css element not at the top level', () => {
-  const Greeting = ({ children, content }) => (
-    <div>
-      {content} {children}
-    </div>
+  jestInCase(
+    'shallow with tickling',
+    ({ render }) => {
+      const wrapper = enzyme.shallow(render())
+      expect(toJson(tickle(wrapper))).toMatchSnapshot()
+    },
+    cases
   )
 
-  const tree = enzyme.mount(
-    <div>
-      <Greeting
-        content={
-          <p id="something" css={{ backgroundColor: 'blue' }}>
-            Hello
-          </p>
-        }
-      >
-        World!
-      </Greeting>
-    </div>
-  )
-  expect(tree).toMatchShallowSnapshot()
-})
-
-test('enzyme test with prop containing css element with other props', () => {
-  const Greeting = ({ children, content }) => (
-    <div>
-      {content} {children}
-    </div>
+  jestInCase(
+    'mount',
+    ({ render }) => {
+      const wrapper = enzyme.mount(render())
+      expect(toJson(wrapper)).toMatchSnapshot()
+    },
+    cases
   )
 
-  const tree = enzyme.mount(
-    <Greeting
-      content={
-        <p id="something" css={{ backgroundColor: 'blue' }}>
-          Hello
-        </p>
-      }
-    >
-      World!
-    </Greeting>
-  )
-  expect(tree).toMatchShallowSnapshot()
-})
+  test('with prop containing css element in fragment', () => {
+    const FragmentComponent = () => (
+      <React.Fragment>
+        x<div css={{ backgroundColor: 'blue' }}>y</div>
+      </React.Fragment>
+    )
 
-test('enzyme test with prop containing css element with other label', () => {
-  const Thing = ({ content, children }) => {
-    return children
-  }
-  const Greeting = ({ children, content }) => (
-    <Thing content={<div css={{ color: 'hotpink' }} />}>
-      {content} {children}
-    </Thing>
-  )
+    const wrapper = enzyme.mount(
+      <div>
+        <FragmentComponent />
+      </div>
+    )
 
-  const tree = enzyme.mount(
-    <Greeting
-      content={
-        <p id="something" css={{ backgroundColor: 'blue' }}>
-          Hello
-        </p>
-      }
-    >
-      World!
-    </Greeting>
-  )
-  expect(tree).toMatchShallowSnapshot()
-})
-
-test('enzyme test with prop containing css element in fragment', () => {
-  const FragmentComponent = () => (
-    <React.Fragment>
-      x<div css={{ backgroundColor: 'blue' }}>y</div>
-    </React.Fragment>
-  )
-
-  const tree = enzyme.mount(
-    <div>
-      <FragmentComponent />
-    </div>
-  )
-  expect(tree).toMatchDeepSnapshot()
+    expect(toJson(wrapper, { mode: 'deep' })).toMatchSnapshot()
+  })
 })
