1bb0cdb38a99fdc559ffb4648d23a4eda44ef18c
mitchellhamilton <mitchell@hamil.town>
2019-12-06 09:46:46 +1100

    Update eslint plugin migration error message

--- removed-packages/eslint-plugin-emotion/src/index.js
+++ removed-packages/eslint-plugin-emotion/src/index.js
@@ -1,3 +1,3 @@
 throw new Error(
-  'The `eslint-plugin-emotion` package has been renamed to `@emotion/eslint-plugin`. Please replace `"plugins": ["emotion"]` with `"plugins": ["@emotion"]` in your ESLint config'
+  'The `eslint-plugin-emotion` package has been renamed to `@emotion/eslint-plugin`. Please replace `"plugins": ["emotion"]` with `"plugins": ["@emotion"]` in your ESLint config.\n if you get an error saying that ESLint could not find the plugin `eslint-plugin-@emotion`, you need to update ESLint.'
 )
