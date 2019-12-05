de5f3098808968120d84562ca72ce479dd0d6d4d
mitchellhamilton <mitchell@hamil.town>
2019-12-06 09:49:42 +1100

    Formatting things

--- removed-packages/eslint-plugin-emotion/src/index.js
+++ removed-packages/eslint-plugin-emotion/src/index.js
@@ -1,3 +1,3 @@
 throw new Error(
-  'The `eslint-plugin-emotion` package has been renamed to `@emotion/eslint-plugin`. Please replace `"plugins": ["emotion"]` with `"plugins": ["@emotion"]` in your ESLint config.\n if you get an error saying that ESLint could not find the plugin `eslint-plugin-@emotion`, you need to update ESLint.'
+  'The `eslint-plugin-emotion` package has been renamed to `@emotion/eslint-plugin`.\nPlease replace `"plugins": ["emotion"]` with `"plugins": ["@emotion"]` in your ESLint config.\nIf you get an error saying that ESLint could not find the plugin `eslint-plugin-@emotion`, you need to update ESLint.'
 )
