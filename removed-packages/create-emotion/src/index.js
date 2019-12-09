ecd0d92942d69b3d2ecf4cfa0620718ca23e1244
Mitchell Hamilton <mitchell@hamil.town>
2019-12-09 13:20:02 +1100

    Update removed-packages/create-emotion/src/index.js
    Co-Authored-By: Mateusz Burzyński <mateuszburzynski@gmail.com>

--- removed-packages/create-emotion/src/index.js
+++ removed-packages/create-emotion/src/index.js
@@ -1,3 +1,3 @@
 throw new Error(
-  "The `create-emotion-server` package has been moved to `@emotion/css/create-instance`. Please import it like this `import createEmotion from '@emotion/css/create-instance'`."
+  "The `create-emotion` package has been moved to `@emotion/css/create-instance`. Please import it like this `import createEmotion from '@emotion/css/create-instance'`."
 )
