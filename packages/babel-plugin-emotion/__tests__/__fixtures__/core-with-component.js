ea69f120a93a50c2f4949375c6b2ca9115ca9066
Norbert de Langen <ndelangen@me.com>
2020-01-20 13:38:08 +0100

    ADD test

---
+++ packages/babel-plugin-emotion/__tests__/__fixtures__/core-with-component.js
@@ -0,0 +1,6 @@
+// @flow
+import styled from '@emotion/styled'
+
+const MyComponent = styled.div({ color: 'hotpink' })
+
+const aliassed = MyComponent.withComponent('section')
--- packages/babel-plugin-emotion/__tests__/index.js
+++ packages/babel-plugin-emotion/__tests__/index.js
@@ -1,6 +1,16 @@
 import babelTester from 'babel-tester'
 import plugin from 'babel-plugin-emotion'
 
+const pluginCopy = t => {
+  const result = { ...plugin(t) }
+
+  result.name = result.name + '-copy'
+
+  return result
+}
+
 babelTester('@emotion/babel-plugin-core', __dirname, {
-  plugins: [plugin]
+  // we add a duplicate of our own plugin
+  // users may run babel twice, and our plugin should be smart enough to not duplicate fields
+  plugins: [plugin, pluginCopy]
 })
