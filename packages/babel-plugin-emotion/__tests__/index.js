ca4a7dddc0aea207b08fe1e5fab5b67fad31b4ea
Norbert de Langen <ndelangen@me.com>
2020-01-21 14:09:09 +0100

    simplify the extra babel-plugin injection

--- packages/babel-plugin-emotion/__tests__/index.js
+++ packages/babel-plugin-emotion/__tests__/index.js
@@ -1,16 +1,8 @@
 import babelTester from 'babel-tester'
 import plugin from 'babel-plugin-emotion'
 
-const pluginCopy = t => {
-  const result = { ...plugin(t) }
-
-  result.name = result.name + '-copy'
-
-  return result
-}
-
 babelTester('@emotion/babel-plugin-core', __dirname, {
   // we add a duplicate of our own plugin
   // users may run babel twice, and our plugin should be smart enough to not duplicate fields
-  plugins: [plugin, pluginCopy]
+  plugins: [plugin, [plugin, undefined, 'emotion-copy']]
 })
