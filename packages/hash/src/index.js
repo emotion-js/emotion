ca0bc9637649b4daf20cd239fc7081535fe92872
Kristóf Poduszló <kripod@protonmail.com>
2020-02-23 03:09:25 +0100

    Optimize Murmur2 for Firefox and Chrome

--- packages/hash/src/index.js
+++ packages/hash/src/index.js
@@ -1,6 +1,6 @@
 // @flow
 /* eslint-disable */
-// Inspired by https://github.com/levitation/murmurhash-js
+// Inspired by https://github.com/garycourt/murmurhash-js
 // Ported from https://github.com/aappleby/smhasher/blob/61a0530f28277f2e850bfc39600ce61d02b518de/src/MurmurHash2.cpp#L37-L86
 
 export default function murmur2(str: string) {
@@ -26,12 +26,16 @@ export default function murmur2(str: string) {
       ((str.charCodeAt(++i) & 0xff) << 16) |
       ((str.charCodeAt(++i) & 0xff) << 24);
 
-    k = /* Math.imul(k, m): */ ((k * 0x5bd1) << 16) + k * 0xe995;
+    k =
+      /* Math.imul(k, m): */
+      (k & 0xffff) * 0x5bd1e995 + (((k >>> 16) * 0xe995) << 16);
     k ^= /* k >>> r: */ k >>> 24;
 
     h =
-      /* Math.imul(k, m): */ (((k * 0x5bd1) << 16) + k * 0xe995) ^
-      /* Math.imul(h, m): */ (((h * 0x5bd1) << 16) + h * 0xe995);
+      /* Math.imul(k, m): */
+      ((k & 0xffff) * 0x5bd1e995 + (((k >>> 16) * 0xe995) << 16)) ^
+      /* Math.imul(h, m): */
+      ((h & 0xffff) * 0x5bd1e995 + (((h >>> 16) * 0xe995) << 16));
   }
 
   // Handle the last few bytes of the input array
@@ -43,14 +47,18 @@ export default function murmur2(str: string) {
       h ^= (str.charCodeAt(i + 1) & 0xff) << 8;
     case 1:
       h ^= str.charCodeAt(i) & 0xff;
-      h = /* Math.imul(h, m): */ ((h * 0x5bd1) << 16) + h * 0xe995;
+      h =
+        /* Math.imul(h, m): */
+        (h & 0xffff) * 0x5bd1e995 + (((h >>> 16) * 0xe995) << 16);
   }
 
   // Do a few final mixes of the hash to ensure the last few
   // bytes are well-incorporated.
 
   h ^= h >>> 13;
-  h = /* Math.imul(h, m): */ ((h * 0x5bd1) << 16) + h * 0xe995;
+  h =
+    /* Math.imul(h, m): */
+    (h & 0xffff) * 0x5bd1e995 + (((h >>> 16) * 0xe995) << 16);
 
   return ((h ^ (h >>> 15)) >>> 0).toString(36);
 }
