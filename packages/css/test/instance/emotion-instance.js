994402e808b5e3e3f0c4e6fec4f7ce9234949109
Mateusz Burzyński <mateuszburzynski@gmail.com>
2020-03-14 19:12:29 +0100

    Bring back accidentally removed tests

---
+++ packages/css/test/instance/emotion-instance.js
@@ -0,0 +1,52 @@
+// @flow
+import createEmotion from '@emotion/css/create-instance'
+import createEmotionServer from '@emotion/server/create-instance'
+import { transform } from 'cssjanus'
+
+function stylisPlugin(context, content) {
+  if (context === 2) {
+    return transform(content)
+  }
+}
+
+export let container
+
+if (typeof document !== 'undefined') {
+  container = document.createElement('div')
+  // $FlowFixMe
+  document.head.appendChild(container)
+}
+
+const emotion = createEmotion({
+  stylisPlugins: stylisPlugin,
+  prefix: (key, value) => {
+    if (key === 'display' && value === 'flex') {
+      return false
+    }
+    return true
+  },
+  nonce: 'some-nonce',
+  key: 'some-key',
+  container
+})
+
+export const {
+  flush,
+  hydrate,
+  cx,
+  merge,
+  getRegisteredStyles,
+  injectGlobal,
+  keyframes,
+  css,
+  sheet,
+  cache
+} = emotion
+
+export const {
+  extractCritical,
+  renderStylesToString,
+  renderStylesToNodeStream
+} = createEmotionServer(cache)
+
+export { default } from '@emotion/styled'
