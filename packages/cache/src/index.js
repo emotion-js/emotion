7b6f70cb02316e2a240cf4531ddc07e088d0bffc
Mateusz Burzyński <mateuszburzynski@gmail.com>
2020-03-16 12:06:31 +0100

    Fix flow errors

--- packages/cache/src/index.js
+++ packages/cache/src/index.js
@@ -51,7 +51,7 @@ let createCache = (options: Options): EmotionCache => {
   if (!key) {
     throw new Error(
       "You have to configure `key` for your cache. Please make sure it's unique (and not equal to 'css') as it's used for linking styles to your cache.\n" +
-        'If multiple caches share the same key they might "fight" for each other\'s style elements.'
+        `If multiple caches share the same key they might "fight" for each other's style elements.`
     )
   }
 
--- scripts/benchmarks/src/implementations/emotion-styled/Provider.js
+++ scripts/benchmarks/src/implementations/emotion-styled/Provider.js
@@ -3,7 +3,7 @@ import * as React from 'react'
 import { CacheProvider } from '@emotion/react'
 import createCache from '@emotion/cache'
 
-let cache = createCache()
+let cache = createCache({ key: 'emo' })
 
 export default (props: { children: React.Node }) => {
   return <CacheProvider value={cache}>{props.children}</CacheProvider>
