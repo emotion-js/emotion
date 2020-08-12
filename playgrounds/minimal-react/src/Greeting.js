0750e8f132d4f446e544e6de68c288fd3f05b783
Ryan Stelly <ryan@skyrisepro.com>
2020-08-11 23:53:54 -0500

    dupe jsx for future rewriting, at least get React's jsx fallback when no css

--- playgrounds/minimal-react/src/Greeting.js
+++ playgrounds/minimal-react/src/Greeting.js
@@ -1,7 +1,10 @@
 // import React from 'react'
 
 const Greeting = ({ message }) => {
-  return <p css={{ color: 'pink' }}>{message}, world!</p>
+  // return <p css={{ color: 'pink' }}>{message}, world!</p>
+  // the following works w/o css prop since we filter that into a passthru to react's jsx
+  // TODO:  make the css prop work 🤞
+  return <p>{message}, world!</p>
 }
 
 export default Greeting
