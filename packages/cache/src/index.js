a2edebb31bbf81fefafc2f0d07c772a98dc1036b
Mateusz Burzyński <mateuszburzynski@gmail.com>
2020-10-14 12:20:13 +0200

    Remove missing key error from production environments

--- packages/cache/src/index.js
+++ packages/cache/src/index.js
@@ -45,7 +45,7 @@ const defaultStylisPlugins = [prefixer]
 let createCache = (options: Options): EmotionCache => {
   let key = options.key
 
-  if (!key) {
+  if (process.env.NODE_ENV !== 'production' && !key) {
     throw new Error(
       "You have to configure `key` for your cache. Please make sure it's unique (and not equal to 'css') as it's used for linking styles to your cache.\n" +
         `If multiple caches share the same key they might "fight" for each other's style elements.`
