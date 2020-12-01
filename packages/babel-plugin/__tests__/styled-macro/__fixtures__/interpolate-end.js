07990c7a627c7fee1cffc721df7a301e3b2124a0
Ian Macalinao <me@ian.pw>
2020-12-01 00:15:32 -0800

    Add regression test

---
+++ packages/babel-plugin/__tests__/styled-macro/__fixtures__/interpolate-end.js
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled/macro'
+
+// yes, this was actually a bug at one point
+const H1 = styled.h1`
+  text-decoration: ${'underline'};
+  border-right: solid blue ${54}px;
+  background: ${'white'};
+  color: ${'black'};
+  display: ${'block'};
+  border-radius: ${'3px'};
+  padding: ${'25px'};
+  width: ${'500px'};
+  z-index: ${100};
+  font-size: ${'18px'};
+  text-align: ${'center'};
+
+  ${props =>
+    css`
+      color: red;
+    `}
+  /* dummy comment */
+  ${props =>
+    css`
+      text-transform: uppercase;
+    `}
+`
