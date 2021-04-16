6c4a9e50f177900f54f7b3368a55b1a7d5e3c002
mitchellhamilton <mitchell@hamil.town>
2021-04-16 13:29:57 +1000

    Fix slack invite link

--- site/src/components/SiteHeader.js
+++ site/src/components/SiteHeader.js
@@ -146,7 +146,7 @@ export default function SiteHeader() {
               </HeaderLink>
             </li>
             <li>
-              <HeaderLink to="https://join.slack.com/t/emotion-slack/shared_invite/zt-l7oe5x0x-2jsjNXOs95f5w4AdZWu9LA">
+              <HeaderLink to="https://join.slack.com/t/emotion-slack/shared_invite/zt-p7lflhc4-mxBbzPbzxoyyuQY54ow0PQ">
                 Slack
               </HeaderLink>
             </li>
