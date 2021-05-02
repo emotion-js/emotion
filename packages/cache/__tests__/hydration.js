4b2329404ef0780b01683c1c0676f21b906aae6c
mitchellhamilton <mitchell@hamil.town>
2021-05-02 13:41:41 +1000

    Fix things

--- packages/cache/__tests__/hydration.js
+++ packages/cache/__tests__/hydration.js
@@ -49,13 +49,43 @@ test('flushing rehydrated styles in the head only affect styles matching the cac
   require('@emotion/react')
 
   const cache = createCache({ key: 'emo' })
-  expect(document.documentElement).toMatchSnapshot()
+  expect(document.documentElement).toMatchInlineSnapshot(`
+    <html>
+      <head>
+        <style
+          data-emotion="css qweqwee"
+          data-s=""
+        >
+          .css-qweqwee{color:red;}
+        </style>
+        <style
+          data-emotion="emo 1lrxbo5"
+          data-s=""
+        >
+          .emo-1lrxbo5{color:hotpink;}
+        </style>
+      </head>
+      <body />
+    </html>
+  `)
 
   cache.sheet.flush()
-  expect(document.documentElement).toMatchSnapshot()
+  expect(document.documentElement).toMatchInlineSnapshot(`
+    <html>
+      <head>
+        <style
+          data-emotion="css qweqwee"
+          data-s=""
+        >
+          .css-qweqwee{color:red;}
+        </style>
+      </head>
+      <body />
+    </html>
+  `)
 })
 
-test('Should only mount style elements matching the cache key', () => {
+test('Should only hydrate style elements matching the cache key', () => {
   let css = `color:hotpink;`
   let hash = hashString(css)
 
@@ -66,12 +96,36 @@ test('Should only mount style elements matching the cache key', () => {
   const cache = createCache({ key: 'css' })
 
   expect(cache.inserted).toEqual({})
-  expect(document.documentElement).toMatchSnapshot()
+  expect(document.documentElement).toMatchInlineSnapshot(`
+    <html>
+      <head>
+        <style
+          data-emotion="emo 1lrxbo5"
+          data-s=""
+        >
+          .css-1lrxbo5{color:hotpink;}
+        </style>
+      </head>
+      <body />
+    </html>
+  `)
 
   const cache2 = createCache({ key: 'emo' })
 
   expect(cache2.inserted).toEqual({ [hash]: true })
-  expect(document.documentElement).toMatchSnapshot()
+  expect(document.documentElement).toMatchInlineSnapshot(`
+    <html>
+      <head>
+        <style
+          data-emotion="emo 1lrxbo5"
+          data-s=""
+        >
+          .css-1lrxbo5{color:hotpink;}
+        </style>
+      </head>
+      <body />
+    </html>
+  `)
 })
 
 describe('In production mode', () => {
@@ -83,7 +137,7 @@ describe('In production mode', () => {
     process.env.NODE_ENV = 'test'
   })
 
-  test('Existing head styles should not be remounted', () => {
+  test('Existing head styles from Emotion 10 should not be moved', () => {
     safeQuerySelector('head').innerHTML = [
       '<style data-emotion="css-global"></style>',
       '<style data-emotion="css"></style>',
@@ -99,6 +153,27 @@ describe('In production mode', () => {
 
     createCache({ key: 'css' })
 
-    expect(document.documentElement).toMatchSnapshot()
+    expect(document.documentElement).toMatchInlineSnapshot(`
+      <html>
+        <head>
+          <style
+            data-emotion="css-global"
+          />
+          <style
+            data-emotion="css"
+          />
+          <style>
+            .my-div{color: red;}
+          </style>
+          <style
+            data-emotion="css 1lrxbo5"
+            data-s=""
+          >
+            .css-1lrxbo5{color:hotpink;}
+          </style>
+        </head>
+        <body />
+      </html>
+    `)
   })
 })
