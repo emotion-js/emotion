ef2bed360dee69f80aaed91f2ef8405e50d92275
Josh Goldberg <me@joshuakgoldberg.com>
2021-05-21 17:59:21 -0400

    Update packages/react/src/theming.js
    Co-authored-by: Mateusz Burzyński <mateuszburzynski@gmail.com>

--- packages/react/src/theming.js
+++ packages/react/src/theming.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import weakMemoize from '@emotion/weak-memoize'
 import hoistNonReactStatics from './isolated-hoist-non-react-statics-do-not-use-this-in-your-code'
 
-export const ThemeContext = /* #__PURE__ */ React.createContext < Object > {}
+export const ThemeContext = /* #__PURE__ */ React.createContext({})
 
 export const useTheme = () => React.useContext(ThemeContext)
 
