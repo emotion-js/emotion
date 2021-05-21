6baaf4f0f27b823ca7901aa452eaa24b6059dc48
Josh Goldberg <me@joshuakgoldberg.com>
2021-05-21 18:24:17 -0400

    Title.js

--- site/src/components/Title.js
+++ site/src/components/Title.js
@@ -2,17 +2,17 @@ import styled from '@emotion/styled'
 import { constants, mq, colors } from '../utils/style'
 import * as markdownComponents from '../utils/markdown-styles'
 
+/*
 type Props = {
   children: React$Node
 }
-export default styled <
-  Props >
-  markdownComponents.h1(
-    mq({
-      paddingTop: 0,
-      marginTop: 0,
-      marginBottom: constants.space[2],
-      fontWeight: 700,
-      color: [colors.hightlight, colors.hightlight, colors.color]
-    })
-  )
+*/
+export default styled(markdownComponents.h1)(
+  mq({
+    paddingTop: 0,
+    marginTop: 0,
+    marginBottom: constants.space[2],
+    fontWeight: 700,
+    color: [colors.hightlight, colors.hightlight, colors.color]
+  })
+)
