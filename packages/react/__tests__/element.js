bc832f26f344ea0510d7e2f36786685f699d44f0
Tyler <tills13@gmail.com>
2021-07-13 11:12:12 -0700

    adjust tests

--- packages/react/__tests__/element.js
+++ packages/react/__tests__/element.js
@@ -2,11 +2,11 @@
 /** @jsx jsx */
 import 'test-utils/dev-mode'
 import { render } from 'react-dom'
-import { Global, jsx, css, CacheProvider, ThemeProvider } from '@emotion/react'
+import { jsx, css, CacheProvider, ThemeProvider } from '@emotion/react'
 import createCache from '@emotion/cache'
-import * as serialize from '@emotion/serialize'
 
-let serializeStyles
+// $FlowFixMe
+console.error = jest.fn()
 
 beforeEach(() => {
   // $FlowFixMe
@@ -15,112 +15,32 @@ beforeEach(() => {
   document.body.innerHTML = `<div id="root"></div>`
 
   jest.clearAllMocks()
-  serializeStyles = jest.spyOn(serialize, 'serializeStyles')
 })
 
-describe('EmotionElement serializeStyles', () => {
-  test('basic', () => {
-    const theme = {}
-
-    // intentionally not using `css` here as it invokes `serializeStyles`
-    render(
-      <ThemeProvider theme={theme}>
-        <CacheProvider value={createCache({ key: 'context' })}>
-          <div css={{ display: 'block' }} />
-        </CacheProvider>
-      </ThemeProvider>,
-      // $FlowFixMe
-      document.getElementById('root')
-    )
-
-    expect(serializeStyles).toHaveBeenNthCalledWith(
-      1,
-      expect.any(Array),
-      undefined,
-      theme
-    )
-  })
-
-  test('basic css', () => {
-    const theme = {}
-
-    // intentionally not using `css` here as it invokes `serializeStyles`
-    render(
-      <ThemeProvider theme={theme}>
-        <CacheProvider value={createCache({ key: 'context' })}>
-          <div
-            css={css`
-              display: block;
-            `}
-          />
-        </CacheProvider>
-      </ThemeProvider>,
-      // $FlowFixMe
-      document.getElementById('root')
-    )
-
-    expect(serializeStyles).toHaveBeenNthCalledWith(
-      2,
-      expect.any(Array),
-      undefined,
-      theme
-    )
-  })
-
-  test('array no theme', () => {
+describe('EmotionElement', () => {
+  test('no React hook order violations', () => {
     const theme = { color: 'blue' }
+    const cache = createCache({ key: 'context' })
 
-    render(
+    // $FlowFixMe
+    const Comp = ({ flag }) => (
       <ThemeProvider theme={theme}>
-        <CacheProvider value={createCache({ key: 'context' })}>
+        <CacheProvider value={cache}>
           <div
-            css={[
-              { display: 'block' },
-              css`
-                color: blue;
-              `
-            ]}
+            css={
+              flag &&
+              (t => css`
+                color: ${t.color};
+              `)
+            }
           />
         </CacheProvider>
-      </ThemeProvider>,
-      // $FlowFixMe
-      document.getElementById('root')
+      </ThemeProvider>
     )
 
-    expect(serializeStyles).toHaveBeenNthCalledWith(
-      2, // 2 as `css` is the first invocation
-      expect.any(Array),
-      undefined,
-      theme
-    )
-  })
-
-  test('array theme', () => {
-    const theme = { color: 'blue' }
-
-    render(
-      <ThemeProvider theme={theme}>
-        <CacheProvider value={createCache({ key: 'context' })}>
-          <div
-            css={[
-              { display: 'block' },
-              t =>
-                css`
-                  color: ${t.color};
-                `
-            ]}
-          />
-        </CacheProvider>
-      </ThemeProvider>,
-      // $FlowFixMe
-      document.getElementById('root')
-    )
-
-    expect(serializeStyles).toHaveBeenNthCalledWith(
-      1, // 1 as `css` is lazily invoked so Global's invocation the first
-      expect.any(Array),
-      undefined,
-      theme
-    )
+    render(<Comp />, document.getElementById('root'))
+    expect(console.error).not.toHaveBeenCalled()
+    render(<Comp flag />, document.getElementById('root'))
+    expect(console.error).not.toHaveBeenCalled()
   })
 })
