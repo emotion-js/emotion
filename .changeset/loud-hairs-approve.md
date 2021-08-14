9ab2cf148ddd7574c56d1937febc037db8623b5c
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-08-14 10:07:12 +0200

    tweak a bunch of stuff

--- .changeset/loud-hairs-approve.md
+++ .changeset/loud-hairs-approve.md
@@ -2,4 +2,4 @@
 '@emotion/weak-memoize': minor
 ---
 
-Convert to TypeScript and emit definitions based on the code
+Source code has been migrated to TypeScript. From now on type declarations will be emitted based on that, instead of being hand-written.
--- packages/weak-memoize/src/index.ts
+++ packages/weak-memoize/src/index.ts
@@ -4,9 +4,9 @@ let weakMemoize = function <Arg extends object, Return>(
   let cache = new WeakMap<Arg, Return>()
   return (arg: Arg) => {
     if (cache.has(arg)) {
-      // Cast to `Return` because we just checked that the cache `has` it
+      // Use non-null assertion because we just checked that the cache `has` it
       // This allows us to remove `undefined` from the return value
-      return cache.get(arg) as Return
+      return cache.get(arg)!
     }
     let ret = func(arg)
     cache.set(arg, ret)
