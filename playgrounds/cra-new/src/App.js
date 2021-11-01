a49bb1994a9dfe3df75b9cc065558c33bd6b57c1
Sam Magura <srmagura@gmail.com>
2021-11-01 16:25:13 -0400

    Add cra-new

---
+++ playgrounds/cra-new/src/App.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+import logo from './logo.svg'
+import './App.css'
+import { jsx } from '@emotion/react/macro'
+
+const MyComponent9 = function MyComponent9() {
+  return <div css={{ color: 'orchid' }}>Foobar</div>
+}
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent9 />
+    </div>
+  )
+}
+
+export default App
