7f4c3cc0faa97c2c779245ef1fc1ea403e660b68
Sam Magura <srmagura@gmail.com>
2021-11-01 16:42:11 -0400

    Redo cra-new

--- playgrounds/cra-new/src/App.js
+++ playgrounds/cra-new/src/App.js
@@ -1,16 +1,9 @@
-/* eslint-disable */
-import logo from './logo.svg'
-import './App.css'
-import { jsx } from '@emotion/react/macro'
-
-const MyComponent9 = function MyComponent9() {
-  return <div css={{ color: 'orchid' }}>Foobar</div>
-}
+/** @jsxImportSource @emotion/react */
 
 function App() {
   return (
     <div className="App">
-      <MyComponent9 />
+      <div css={{ color: 'orchid' }}>create-react-app</div>
     </div>
   )
 }
