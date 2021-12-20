5a98d24d99b7927dea726fe0b96c79356ff25577
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-12-20 16:27:17 +0100

    Fixed import-prod test problem with production React 18 throwing when used with RTL cause it's using act

--- packages/react/__tests__/import-prod.js
+++ packages/react/__tests__/import-prod.js
@@ -1,9 +1,9 @@
 // @flow
 import 'test-utils/prod-mode'
 import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 import { css, Global } from '@emotion/react'
 import styled from '@emotion/styled'
-import { render } from '@testing-library/react'
 import prettify from '@emotion/css-prettifier'
 
 // using styled instead of the css prop because there was a really weird flow error
@@ -21,8 +21,22 @@ expect.addSnapshotSerializer({
   }
 })
 
-test('it works', () => {
-  render(
+// can't use RTL as production React 18 throws when it's trying to use `act`
+const render = children =>
+  new Promise(resolve => {
+    const el = document.createElement('div')
+    document.body.appendChild(el)
+
+    if ((ReactDOM: any).createRoot) {
+      const root = (ReactDOM: any).createRoot(el)
+      root.render(<div ref={resolve}>{children}</div>)
+    } else {
+      ReactDOM.render(children, el, resolve)
+    }
+  })
+
+test('it works', async () => {
+  await render(
     <div>
       <Comp>something</Comp>
 
