1a84bee8e9b532d7005dfc6da7c3bb16abf57d32
Sam Magura <srmagura@gmail.com>
2021-12-30 13:55:22 -0500

    Website: Live code editor stuff

--- site/components/live-editor/compile.ts
+++ site/components/live-editor/compile.ts
@@ -1,58 +1,18 @@
-import {
-  BabelFileResult,
-  CompileFailureMessage,
-  CompileRequestMessage,
-  CompileSuccessMessage
-} from './types'
+import * as Babel from '@babel/standalone'
 
-const worker = new Worker(new URL('./babel-worker.js', import.meta.url))
+const options = {
+  presets: [
+    Babel.availablePresets['env'],
+    [
+      Babel.availablePresets['react'],
+      { runtime: 'automatic', importSource: '@emotion/react' }
+    ]
+  ]
+}
 
-let count = 0
-
-export function compile(code: string): Promise<BabelFileResult> {
-  return new Promise((resolve, reject) => {
-    const id = ++count
-
-    function messageHandler({
-      data
-    }: MessageEvent<CompileSuccessMessage | CompileFailureMessage>): void {
-      if (data.id !== id) return
-
-      worker.removeEventListener('message', messageHandler)
-
-      if (data.type === 'success') {
-        resolve(data.result)
-      } else {
-        if (
-          !(
-            data.error &&
-            typeof (data.error as { message?: unknown }).message === 'string'
-          )
-        ) {
-          reject(new Error('Unknown compilation error.'))
-          return
-        }
-
-        const workerError = data.error as {
-          message: string
-          [key: string]: unknown
-        }
-        const error = new Error(workerError.message)
-
-        for (const [key, value] of Object.entries(
-          data.error as Record<string, unknown>
-        )) {
-          if (key === 'message') continue
-          ;(error as unknown as Record<string, unknown>)[key] = value
-        }
-
-        reject(error)
-      }
-    }
-
-    worker.addEventListener('message', messageHandler)
-
-    const requestMessage: CompileRequestMessage = { id, code }
-    worker.postMessage(requestMessage)
-  })
+export function compile(code: string): string {
+  const result = Babel.transform(code, options).code
+  if (!result) throw new Error('Babel failed to compile the code.')
+  console.log(result)
+  return result
 }
