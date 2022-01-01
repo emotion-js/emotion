ed556c24ad6ef8486428518e298276bf15b47dad
Sam Magura <srmagura@gmail.com>
2022-01-01 09:28:18 -0500

    Done with live code editors

--- site/components/live-editor/emotion-live-editor.tsx
+++ site/components/live-editor/emotion-live-editor.tsx
@@ -10,6 +10,37 @@ import { css } from '@emotion/react'
 import { colors, styleConstants } from '../../util'
 import { compile } from './compile'
 
+const scope = {
+  process: {
+    env: {
+      NODE_ENV: process.env.NODE_ENV
+    }
+  },
+  require(moduleName: string) {
+    switch (moduleName) {
+      case '@emotion/css':
+        return require('@emotion/css')
+      case '@emotion/cache':
+        return require('@emotion/cache')
+      case '@emotion/react':
+        return require('@emotion/react')
+      case '@emotion/react/jsx-runtime':
+        return require('@emotion/react/jsx-runtime')
+      case '@emotion/styled':
+        return require('@emotion/styled')
+      case '@emotion/styled/base':
+        return require('@emotion/styled/base')
+      case '@emotion/is-prop-valid':
+        return require('@emotion/is-prop-valid')
+      case 'facepaint':
+        return require('facepaint')
+      default:
+        // eslint-disable-next-line no-throw-literal
+        throw `Module "${moduleName}" not found.`
+    }
+  }
+}
+
 const borderRadius = '0.5rem'
 
 const theCss = {
@@ -20,6 +51,7 @@ const theCss = {
   editor: css({
     flex: 1,
     backgroundColor: 'rgb(40, 41, 54)', // Copied from Prism theme
+    caretColor: 'white',
     fontSize: styleConstants.fontSizeSm,
 
     borderStartStartRadius: borderRadius,
@@ -28,7 +60,9 @@ const theCss = {
 
   rightColumn: css({
     flex: 1,
-    padding: '1rem',
+    padding: '0.5rem',
+    display: 'flex',
+    flexDirection: 'column',
 
     border: `1px solid ${colors.grayBorder}`,
     borderLeftStyle: 'none',
@@ -36,14 +70,22 @@ const theCss = {
     borderEndEndRadius: borderRadius
   }),
 
+  label: css({
+    textAlign: 'center',
+    fontSize: styleConstants.fontSizeSm,
+    color: colors.gray500
+  }),
+
   error: css({
+    flex: 1,
+    padding: '0.5rem',
     marginBottom: 0,
     color: colors.danger
   }),
 
   preview: css({
     flex: 1,
-    padding: '1rem',
+    padding: '0.5rem',
 
     display: 'flex',
     alignItems: 'center',
@@ -62,25 +104,27 @@ const noTheme: EditorProps['theme'] = {
 
 interface LiveEditorProps {
   code: string
-  language: 'jsx' | 'tsx'
 }
 
-export function EmotionLiveEditor({
-  code,
-  language
-}: LiveEditorProps): ReactElement {
+export function EmotionLiveEditor({ code }: LiveEditorProps): ReactElement {
   return (
-    <div className="emotion-live-editor" css={theCss.container}>
-      <LiveProvider
-        code={code}
-        language={language}
-        noInline
-        transformCode={compile}
-      >
-        <LiveEditor css={theCss.editor} theme={noTheme} />
-        <LiveError css={[theCss.rightColumn, theCss.error]} />
-        <LivePreview css={[theCss.rightColumn, theCss.preview]} />
-      </LiveProvider>
+    <div className="emotion-live-editor">
+      <div css={theCss.container}>
+        <LiveProvider
+          code={code}
+          language="jsx"
+          noInline
+          transformCode={compile}
+          scope={scope}
+        >
+          <LiveEditor css={theCss.editor} theme={noTheme} />
+          <div css={theCss.rightColumn}>
+            <LiveError css={theCss.error} />
+            <LivePreview css={theCss.preview} />
+            <div css={theCss.label}>(Edit code to see changes)</div>
+          </div>
+        </LiveProvider>
+      </div>
     </div>
   )
 }
