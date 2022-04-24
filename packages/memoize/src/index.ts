b9640a5b55144c6c1722a129c6b2ecc18d4d1dd1
Sam Magura <srmagura@gmail.com>
2022-04-24 12:35:53 -0400

    Fix unintentional breaking change to @emotion/memoize

--- packages/memoize/src/index.ts
+++ packages/memoize/src/index.ts
@@ -1,6 +1,6 @@
-export default function memoize<K extends PropertyKey, V>(
-  fn: (arg: K) => V
-): (arg: K) => V {
+function memoize<V>(fn: (arg: string) => V): (arg: string) => V
+function memoize<K extends PropertyKey, V>(fn: (arg: K) => V): (arg: K) => V
+function memoize<K extends PropertyKey, V>(fn: (arg: K) => V): (arg: K) => V {
   const cache: Record<K, V> = Object.create(null)
 
   return (arg: K) => {
@@ -8,3 +8,5 @@ export default function memoize<K extends PropertyKey, V>(
     return cache[arg]
   }
 }
+
+export default memoize
--- packages/memoize/types/tests.ts
+++ packages/memoize/types/tests.ts
@@ -1,5 +1,8 @@
 import memoize from '@emotion/memoize'
 
+memoize<boolean>((arg: string) => !!arg)
+memoize<number, boolean>((arg: number) => !!arg)
+
 // $ExpectType string[]
 memoize((arg: string) => [arg])('foo')
 
