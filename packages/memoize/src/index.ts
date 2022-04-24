f187b1d4da550ffbff32bb8d26ed7bdfa4237afe
Sam Magura <srmagura@gmail.com>
2022-04-24 13:16:01 -0400

    Reorder function overloads in packages/memoize/src/index.ts
    Co-authored-by: Mateusz Burzyński <mateuszburzynski@gmail.com>

--- packages/memoize/src/index.ts
+++ packages/memoize/src/index.ts
@@ -1,5 +1,5 @@
-function memoize<V>(fn: (arg: string) => V): (arg: string) => V
 function memoize<K extends PropertyKey, V>(fn: (arg: K) => V): (arg: K) => V
+function memoize<V>(fn: (arg: string) => V): (arg: string) => V
 function memoize<K extends PropertyKey, V>(fn: (arg: K) => V): (arg: K) => V {
   const cache: Record<K, V> = Object.create(null)
 
