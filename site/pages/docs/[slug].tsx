7ec7d2e38f6db4d624eedc0dad28bff9182ddf94
Sam Magura <srmagura@gmail.com>
2022-04-30 14:33:06 -0400

    site: Upgrade next-mdx-remote

--- site/pages/docs/[slug].tsx
+++ site/pages/docs/[slug].tsx
@@ -118,7 +118,7 @@ export default function DocsPage({
           {...mdx}
           components={{
             table: ResponsiveTable,
-            'live-editor': EmotionLiveEditor
+            EmotionLiveEditor
           }}
         />
       </DocWrapper>
