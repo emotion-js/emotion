6c0c77c74c36e6c5b7258a718a12d8e1e9b40b7a
Sam Magura <srmagura@gmail.com>
2022-07-14 09:24:44 -0400

    Update remark-fix-links to handle github/emotion-js/emotion links

--- site/util/remark-fix-links.ts
+++ site/util/remark-fix-links.ts
@@ -3,9 +3,13 @@ import { visit } from 'unist-util-visit'
 export function remarkFixLinks() {
   return (markdownAST: any) => {
     visit(markdownAST, 'link', (node: { url: string }) => {
-      node.url = node.url.replace(/^https?:\/\/emotion.sh/, '')
+      node.url = node.url.replace(/^https?:\/\/emotion.sh/i, '')
+      node.url = node.url.replace(
+        /^https?:\/\/github.com\/emotion-js\/emotion\/tree\/main/i,
+        ''
+      )
 
-      if (!node.url.startsWith('//') && !node.url.startsWith('http')) {
+      if (!node.url.startsWith('http')) {
         node.url = node.url
           .replace(/\.mdx?(#.*)?$/, (_, hash) => {
             return hash || ''
