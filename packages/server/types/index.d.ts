5291b68667a382fe61f3e92a63f52d0a772f724f
manish <me@manish.co>
2022-07-22 11:53:50 +0100

    feat: update @emotion/server types to support all exported members

--- packages/server/types/index.d.ts
+++ packages/server/types/index.d.ts
@@ -3,6 +3,8 @@
 
 import { EmotionServer } from '@emotion/server/create-instance'
 
+export const extractCritical: EmotionServer['extractCritical']
+export const extractCriticalToChunks: EmotionServer['extractCriticalToChunks']
 export const renderStylesToString: EmotionServer['renderStylesToString']
 export const renderStylesToNodeStream: EmotionServer['renderStylesToNodeStream']
-export const extractCritical: EmotionServer['extractCritical']
+export const constructStyleTagsFromChunks: EmotionServer['constructStyleTagsFromChunks']
