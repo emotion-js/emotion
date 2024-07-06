4af435e57ca238d5d7aa4463fc794abb2f0e441a
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-07-06 23:36:38 +0200

    simplify one thing

--- packages/react/types/jsx-namespace.d.ts
+++ packages/react/types/jsx-namespace.d.ts
@@ -8,38 +8,28 @@ type WithConditionalCSSProp<P> = 'className' extends keyof P
     : {}
   : {}
 
-// unpack all here to avoid infinite self-referencing when defining our own JSX namespace
-type ReactJSXElement = React.JSX.Element
-type ReactJSXElementClass = React.JSX.ElementClass
-type ReactJSXElementAttributesProperty = React.JSX.ElementAttributesProperty
-type ReactJSXElementChildrenAttribute = React.JSX.ElementChildrenAttribute
-type ReactJSXLibraryManagedAttributes<C, P> =
-  React.JSX.LibraryManagedAttributes<C, P>
-type ReactJSXIntrinsicAttributes = React.JSX.IntrinsicAttributes
-type ReactJSXIntrinsicClassAttributes<T> = React.JSX.IntrinsicClassAttributes<T>
-type ReactJSXIntrinsicElements = React.JSX.IntrinsicElements
-
 // based on the code from @types/react@18.2.8
 // https://github.com/DefinitelyTyped/DefinitelyTyped/blob/3197efc097d522c4bf02b94e1a0766d007d6cdeb/types/react/index.d.ts#LL3204C13-L3204C13
 type ReactJSXElementType = string | React.JSXElementConstructor<any>
 
 export namespace EmotionJSX {
   type ElementType = ReactJSXElementType
-  interface Element extends ReactJSXElement {}
-  interface ElementClass extends ReactJSXElementClass {}
+  interface Element extends React.JSX.Element {}
+  interface ElementClass extends React.JSX.ElementClass {}
   interface ElementAttributesProperty
-    extends ReactJSXElementAttributesProperty {}
-  interface ElementChildrenAttribute extends ReactJSXElementChildrenAttribute {}
+    extends React.JSX.ElementAttributesProperty {}
+  interface ElementChildrenAttribute
+    extends React.JSX.ElementChildrenAttribute {}
 
   type LibraryManagedAttributes<C, P> = WithConditionalCSSProp<P> &
-    ReactJSXLibraryManagedAttributes<C, P>
+    React.JSX.LibraryManagedAttributes<C, P>
 
-  interface IntrinsicAttributes extends ReactJSXIntrinsicAttributes {}
+  interface IntrinsicAttributes extends React.JSX.IntrinsicAttributes {}
   interface IntrinsicClassAttributes<T>
-    extends ReactJSXIntrinsicClassAttributes<T> {}
+    extends React.JSX.IntrinsicClassAttributes<T> {}
 
   type IntrinsicElements = {
-    [K in keyof ReactJSXIntrinsicElements]: ReactJSXIntrinsicElements[K] & {
+    [K in keyof React.JSX.IntrinsicElements]: React.JSX.IntrinsicElements[K] & {
       css?: Interpolation<Theme>
     }
   }
