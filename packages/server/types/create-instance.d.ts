d0610b5f99a8efa07614bcb1068f84b82c2dedc3
Emma Hamilton <git@emmas.town>
2024-07-25 09:31:04 +1000

    Update packages/server/types/create-instance.d.ts

--- packages/server/types/create-instance.d.ts
+++ packages/server/types/create-instance.d.ts
@@ -1,7 +1,7 @@
 // Definitions by: Junyoung Clare Jang <https://github.com/Ailrun>
 // TypeScript Version: 2.8
 
-/// <reference types="node" preverve="true" />
+/// <reference types="node" preserve="true" />
 import { EmotionCache } from '@emotion/utils'
 
 export interface EmotionCritical {
