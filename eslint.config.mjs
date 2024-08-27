34079f5b2330f87855e03f8a79dc3e84ec4cd8df
Nicholas Narsing <soren121@sorenstudios.com>
2024-08-27 15:49:17 -0700

    Switch monorepo to ESLint 9 & flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,61 @@
+import globals from 'globals'
+import prettierRecommended from 'eslint-plugin-prettier/recommended'
+import emotion from '@emotion/eslint-plugin'
+import react from 'eslint-plugin-react'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  tseslint.configs.eslintRecommended,
+  {
+    ignores: [
+      '**/lib/',
+      '**/dist/',
+      '**/coverage/',
+      '**/node_modules/',
+      '**/stylis.min.js',
+      'demo/dist',
+      'site/out'
+    ],
+    plugins: {
+      '@emotion': emotion,
+      react
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      globals: {
+        ...globals.browser
+      }
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: 0
+    },
+    rules: {
+      camelcase: 0,
+      'no-template-curly-in-string': 0,
+      'prefer-const': 0,
+      'no-unused-vars': 0,
+      'prettier/prettier': [
+        'error',
+        {
+          parser: 'typescript'
+        }
+      ],
+      'react/jsx-curly-brace-presence': 0,
+      'react/jsx-handler-names': 0,
+      'react/no-unused-prop-types': 0,
+      'react/prop-types': 0,
+      'react/react-in-jsx-scope': 0,
+      '@emotion/pkg-renaming': 2
+    }
+  },
+  {
+    files: ['**/*.test.js', '**/__tests__/**'],
+    ignores: ['**/__fixtures__/*'],
+    languageOptions: {
+      globals: {
+        ...globals.jest
+      }
+    }
+  },
+  prettierRecommended
+)
